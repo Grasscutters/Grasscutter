2e01c7d5ad5f46a6bb504b38d1ad8bc2c9a0ec16
KingRainbow44 <kobedo11@gmail.com>
2022-04-18 22:23:22 -0400

    Find by player's UID not IID

--- src/main/java/emu/grasscutter/commands/ServerCommands.java
+++ src/main/java/emu/grasscutter/commands/ServerCommands.java
@@ -42,7 +42,7 @@ public final class ServerCommands {
                 int target = Integer.parseInt(args.get(0));
                 String message = String.join(" ", args.subList(1, args.size()));
 
-                GenshinPlayer targetPlayer = Grasscutter.getGameServer().getPlayerById(target);
+                GenshinPlayer targetPlayer = Grasscutter.getGameServer().getPlayerByUid(target);
                 if(targetPlayer == null) {
                     CommandHandler.sendMessage(null, "Player not found."); return;
                 }
@@ -64,7 +64,7 @@ public final class ServerCommands {
                 int target = Integer.parseInt(args.get(0));
                 String message = String.join(" ", args.subList(1, args.size()));
 
-                GenshinPlayer targetPlayer = Grasscutter.getGameServer().getPlayerById(target);
+                GenshinPlayer targetPlayer = Grasscutter.getGameServer().getPlayerByUid(target);
                 if(targetPlayer == null) {
                     CommandHandler.sendMessage(null, "Player not found."); return;
                 }
@@ -108,7 +108,7 @@ public final class ServerCommands {
                     Account account = DatabaseHelper.createAccountWithId(username, uid);
                     if(account == null) {
                         CommandHandler.sendMessage(null, "Account already exists."); return;
-                    } else CommandHandler.sendMessage(null, "Account created with UID " + account.getId() + ".");
+                    } else CommandHandler.sendMessage(null, "Account created with UID " + account.getPlayerId() + ".");
                     return;
                 case "delete":
                     if(DatabaseHelper.deleteAccount(username)) {
