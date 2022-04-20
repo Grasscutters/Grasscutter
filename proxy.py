087cd680bd3fcf7c0f6fa22d86eada7313b6cdcb
Jaida Wu <mlgmxyysd@meowcat.org>
2022-04-20 18:13:26 +0800

    Optimize proxy scripts
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

--- proxy.py
+++ proxy.py
@@ -21,43 +21,45 @@
 ##
 
 from mitmproxy import http
+from proxy_config import REMOTE_HOST
 
 class MlgmXyysd_Genshin_Impact_Proxy:
 
+    LIST_DOMAINS = [
+        "api-os-takumi.mihoyo.com",
+        "hk4e-api-os-static.mihoyo.com",
+        "hk4e-sdk-os.mihoyo.com",
+        "dispatchosglobal.yuanshen.com",
+        "osusadispatch.yuanshen.com",
+        "account.mihoyo.com",
+        "log-upload-os.mihoyo.com",
+        "dispatchcntest.yuanshen.com",
+        "devlog-upload.mihoyo.com",
+        "webstatic.mihoyo.com",
+        "log-upload.mihoyo.com",
+        "hk4e-sdk.mihoyo.com",
+        "api-beta-sdk.mihoyo.com",
+        "api-beta-sdk-os.mihoyo.com",
+        "cnbeta01dispatch.yuanshen.com",
+        "dispatchcnglobal.yuanshen.com",
+        "cnbeta02dispatch.yuanshen.com",
+        "sdk-os-static.mihoyo.com",
+        "webstatic-sea.mihoyo.com",
+        "webstatic-sea.hoyoverse.com",
+        "hk4e-sdk-os-static.hoyoverse.com",
+        "sdk-os-static.hoyoverse.com",
+        "api-account-os.hoyoverse.com",
+        "hk4e-sdk-os.hoyoverse.com",
+        "overseauspider.yuanshen.com",
+        "gameapi-account.mihoyo.com",
+        "minor-api.mihoyo.com",
+        "public-data-api.mihoyo.com",
+        "uspider.yuanshen.com",
+        "sdk-static.mihoyo.com"
+    ]
+
     def request(self, flow: http.HTTPFlow) -> None:
-        
-        # This can also be replaced with another IP address.
-        REMOTE_HOST = "localhost"
-        
-        LIST_DOMAINS = [
-            "api-os-takumi.mihoyo.com",
-            "hk4e-api-os-static.mihoyo.com",
-            "hk4e-sdk-os.mihoyo.com",
-            "dispatchosglobal.yuanshen.com",
-            "osusadispatch.yuanshen.com",
-            "account.mihoyo.com",
-            "log-upload-os.mihoyo.com",
-            "dispatchcntest.yuanshen.com",
-            "devlog-upload.mihoyo.com",
-            "webstatic.mihoyo.com",
-            "log-upload.mihoyo.com",
-            "hk4e-sdk.mihoyo.com",
-            "api-beta-sdk.mihoyo.com",
-            "api-beta-sdk-os.mihoyo.com",
-            "cnbeta01dispatch.yuanshen.com",
-            "dispatchcnglobal.yuanshen.com",
-            "cnbeta02dispatch.yuanshen.com",
-            "sdk-os-static.mihoyo.com",
-            "webstatic-sea.mihoyo.com",
-            "webstatic-sea.hoyoverse.com",
-            "hk4e-sdk-os-static.hoyoverse.com",
-            "sdk-os-static.hoyoverse.com",
-            "api-account-os.hoyoverse.com",
-            "hk4e-sdk-os.hoyoverse.com",
-            "overseauspider.yuanshen.com"
-        ]
-        
-        if flow.request.host in LIST_DOMAINS:
+        if flow.request.host in self.LIST_DOMAINS:
             flow.request.host = REMOTE_HOST
 
 addons = [
