c42fa4ee5d5ef1ad25a616c1501a80e1d5c7afe7
Jaida Wu <mlgmxyysd@meowcat.org>
2022-04-20 20:58:57 +0800

    Add pos command
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ src/main/java/emu/grasscutter/command/commands/Pos.java
@@ -0,0 +1,22 @@
+package emu.grasscutter.command.commands;
+
+import emu.grasscutter.command.Command;
+import emu.grasscutter.command.CommandHandler;
+import emu.grasscutter.game.GenshinPlayer;
+
+import java.util.List;
+
+@Command(label = "pos",
+        usage = "Usage: pos", description = "Get coordinates.")
+public class Pos implements CommandHandler {
+
+    @Override
+    public void onCommand(GenshinPlayer sender, List<String> args) {
+        if (sender == null) {
+            CommandHandler.sendMessage(null, "Run this command in-game.");
+            return;
+        }
+
+        sender.dropMessage(String.format("Coord: %.3f, %.3f, %.3f", sender.getPos().getX(), sender.getPos().getY(), sender.getPos().getZ()));
+    }
+}
