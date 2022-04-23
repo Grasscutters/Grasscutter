45281695feaffad71d77921eec321e7424cb478e
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-04-23 21:03:29 +0800

    Try to give name card

---
+++ src/main/java/emu/grasscutter/data/common/RewardItemData.java
@@ -0,0 +1,22 @@
+package emu.grasscutter.data.common;
+
+public class RewardItemData {
+    private int ItemId;
+    private int ItemCount;
+
+    public int getItemId() {
+        return ItemId;
+    }
+
+    public void setItemId(int itemId) {
+        ItemId = itemId;
+    }
+
+    public int getItemCount() {
+        return ItemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        ItemCount = itemCount;
+    }
+}
