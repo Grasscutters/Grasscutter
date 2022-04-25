ba76c161827aa29404214eab5406b4ad90ac6ad7
wetabq <wetabq@gmail.com>
2022-04-25 19:26:25 +0800

    Code style clean up

--- src/main/java/emu/grasscutter/data/LuaSceneDataLoader.kt
+++ src/main/java/emu/grasscutter/data/LuaSceneDataLoader.kt
@@ -9,282 +9,284 @@ import java.util.concurrent.ConcurrentHashMap
 
 
 val jsonFormatter = Json {
-    prettyPrint = true
-    ignoreUnknownKeys = true
-    coerceInputValues = true
-    encodeDefaults = true
+	prettyPrint = true
+	ignoreUnknownKeys = true
+	coerceInputValues = true
+	encodeDefaults = true
 }
 
 object LuaSceneDataLoader {
 
-    @JvmField
-    val scenes: ConcurrentHashMap<Int, Scene> = ConcurrentHashMap()
+	@JvmField
+	val scenes: ConcurrentHashMap<Int, Scene> = ConcurrentHashMap()
 
-    @JvmField
-    val entityCampData: ConcurrentHashMap<Int, Int> = ConcurrentHashMap()
+	@JvmField
+	val entityCampData: ConcurrentHashMap<Int, Int> = ConcurrentHashMap()
 
-    @JvmStatic
-    fun init() {
-        val scenePath = Grasscutter.getConfig().RESOURCE_FOLDER + "Lua/Scene/"
-        val directory = File(scenePath)
-        directory.listFiles().forEach { sceneFile ->
-            sceneFile.path.takeLastWhile { it.isDigit() }.toIntOrNull()?.also { sceneId ->
-                val sceneDataLoader = SceneDataLoader(sceneFile.path, sceneId)
-                val scene = sceneDataLoader.scene
-                scenes[sceneId] = scene
-            }
-        }
+	@JvmStatic
+	fun init() {
+		val scenePath = Grasscutter.getConfig().RESOURCE_FOLDER + "Lua/Scene/"
+		val directory = File(scenePath)
+		directory.listFiles().forEach { sceneFile ->
+			sceneFile.path.takeLastWhile { it.isDigit() }.toIntOrNull()?.also { sceneId ->
+				val sceneDataLoader = SceneDataLoader(sceneFile.path, sceneId)
+				val scene = sceneDataLoader.scene
+				scenes[sceneId] = scene
+			}
+		}
 
-        val entityCampDataPath = listOf(
-            Grasscutter.getConfig().RESOURCE_FOLDER + "ExcelBinOutput/MonsterExcelConfigData.json",
-            Grasscutter.getConfig().RESOURCE_FOLDER + "ExcelBinOutput/GadgetExcelConfigData.json",
-            Grasscutter.getConfig().RESOURCE_FOLDER + "ExcelBinOutput/NpcExcelConfigData.json")
-        entityCampDataPath.forEach { path ->
-            val data = jsonFormatter.decodeFromString<List<EntityCampData>>(
-                File(path).inputStream().bufferedReader().use(BufferedReader::readText)
-            )
-            data.forEach { campData ->
-                campData.campId?.also {
-                    entityCampData[campData.id] = campData.campId
-                }
-            }
-        }
-    }
+		val entityCampDataPath = listOf(
+			Grasscutter.getConfig().RESOURCE_FOLDER + "ExcelBinOutput/MonsterExcelConfigData.json",
+			Grasscutter.getConfig().RESOURCE_FOLDER + "ExcelBinOutput/GadgetExcelConfigData.json",
+			Grasscutter.getConfig().RESOURCE_FOLDER + "ExcelBinOutput/NpcExcelConfigData.json"
+		)
+		entityCampDataPath.forEach { path ->
+			val data = jsonFormatter.decodeFromString<List<EntityCampData>>(
+				File(path).inputStream().bufferedReader().use(BufferedReader::readText)
+			)
+			data.forEach { campData ->
+				campData.campId?.also {
+					entityCampData[campData.id] = campData.campId
+				}
+			}
+		}
+	}
 
 }
 
 @Serializable
 class EntityCampData(
-    @SerialName("Id") val id: Int,
-    @SerialName("CampID") val campId: Int? = null
+	@SerialName("Id") val id: Int,
+	@SerialName("CampID") val campId: Int? = null
 )
 
 class SceneDataLoader(
-    val scenePath: String,
-    val sceneId: Int,
+	val scenePath: String,
+	val sceneId: Int,
 ) {
 
-    private val sceneFile = File("$scenePath/scene$sceneId.json")
-    val scene by lazy {
+	private val sceneFile = File("$scenePath/scene$sceneId.json")
+	val scene by lazy {
 //        println("Current decoding ${sceneFile.path}")
-        jsonFormatter.decodeFromString<Scene>(sceneFile.readText()).apply { sceneDataLoader = this@SceneDataLoader }
-    }
+		jsonFormatter.decodeFromString<Scene>(sceneFile.readText()).apply { sceneDataLoader = this@SceneDataLoader }
+	}
 
 
-    private fun getBlockFile(blockId: Int): File {
-        return File("${scenePath}/scene${sceneId}_block${blockId}.json")
-    }
+	private fun getBlockFile(blockId: Int): File {
+		return File("${scenePath}/scene${sceneId}_block${blockId}.json")
+	}
 
-    fun getBlock(blockId: Int): Block {
+	fun getBlock(blockId: Int): Block {
 //        println("Current decoding ${getBlockFile(blockId)}")
-        return jsonFormatter.decodeFromString<Block>(getBlockFile(blockId).readText()).apply { sceneDataLoader = this@SceneDataLoader }
-    }
+		return jsonFormatter.decodeFromString<Block>(getBlockFile(blockId).readText())
+			.apply { sceneDataLoader = this@SceneDataLoader }
+	}
 
-    private fun getGroupFile(groupId: Long): File {
-        return File("${scenePath}/scene${sceneId}_group${groupId}.json")
-    }
+	private fun getGroupFile(groupId: Long): File {
+		return File("${scenePath}/scene${sceneId}_group${groupId}.json")
+	}
 
-    fun getGroup(groupId: Long): Group {
+	fun getGroup(groupId: Long): Group {
 //        println("Current decoding ${getGroupFile(groupId)}")
-        return jsonFormatter.decodeFromString(getGroupFile(groupId).readText())
-    }
+		return jsonFormatter.decodeFromString(getGroupFile(groupId).readText())
+	}
 
 }
 
 
 @Serializable
 data class Scene(
-    @Transient var sceneDataLoader: SceneDataLoader? = null,
-    @SerialName("blocks") val rawBlocks: List<Int>,
-    @SerialName("block_rects") val blockRectangles: List<BlockRectangle> = emptyList(),
-    @SerialName("scene_config") val sceneConfig: SceneConfig,
+	@Transient var sceneDataLoader: SceneDataLoader? = null,
+	@SerialName("blocks") val rawBlocks: List<Int>,
+	@SerialName("block_rects") val blockRectangles: List<BlockRectangle> = emptyList(),
+	@SerialName("scene_config") val sceneConfig: SceneConfig,
 ) {
-    val block2Rectangle: Map<Int, BlockRectangle> by lazy {
-        rawBlocks.zip(blockRectangles).toMap()
-    }
+	val block2Rectangle: Map<Int, BlockRectangle> by lazy {
+		rawBlocks.zip(blockRectangles).toMap()
+	}
 
-    val blocks: Map<Int, Block> by lazy {
-        sceneDataLoader?.let { sceneDataLoader ->
-            rawBlocks.associateWith { id -> sceneDataLoader.getBlock(id) }
-        } ?: throw NullPointerException()
-    }
+	val blocks: Map<Int, Block> by lazy {
+		sceneDataLoader?.let { sceneDataLoader ->
+			rawBlocks.associateWith { id -> sceneDataLoader.getBlock(id) }
+		} ?: throw NullPointerException()
+	}
 
-    fun getPlayerBlock(pos: Vector2): Map<Int, Block> {
-        return block2Rectangle.filter { entry -> entry.value.contains(pos) }.mapValues { blocks[it.key]!! }
-    }
+	fun getPlayerBlock(pos: Vector2): Map<Int, Block> {
+		return block2Rectangle.filter { entry -> entry.value.contains(pos) }.mapValues { blocks[it.key]!! }
+	}
 
 }
 
 @Serializable
 data class SceneConfig(
-    @SerialName("begin_pos") val beginPos: Vector2,
-    @SerialName("born_pos") val bornPos: Vector3,
-    @SerialName("born_rot") val bornRot: Vector3,
-    @SerialName("die_y") val dieY: Double? = null,
-    val size: Vector2,
-    @SerialName("vision_anchor") val visionAnchor: Vector2,
+	@SerialName("begin_pos") val beginPos: Vector2,
+	@SerialName("born_pos") val bornPos: Vector3,
+	@SerialName("born_rot") val bornRot: Vector3,
+	@SerialName("die_y") val dieY: Double? = null,
+	val size: Vector2,
+	@SerialName("vision_anchor") val visionAnchor: Vector2,
 )
 
 @Serializable
-data class Block (
-    @Transient var sceneDataLoader: SceneDataLoader? = null,
-    @SerialName("groups") val groupInfo: List<GroupInfo>
+data class Block(
+	@Transient var sceneDataLoader: SceneDataLoader? = null,
+	@SerialName("groups") val groupInfo: List<GroupInfo>
 ) {
-    val groups: Map<Long, Group> by lazy {
-        sceneDataLoader?.let { sceneDataLoader ->
-            groupInfo.associate { info -> info.id to sceneDataLoader.getGroup(info.id) }
-        } ?: throw NullPointerException()
-    }
+	val groups: Map<Long, Group> by lazy {
+		sceneDataLoader?.let { sceneDataLoader ->
+			groupInfo.associate { info -> info.id to sceneDataLoader.getGroup(info.id) }
+		} ?: throw NullPointerException()
+	}
 }
 
 @Serializable
 data class GroupInfo(
-    val area: Int? = null,
-    val id: Long,
-    @SerialName("dynamic_load") val dynamicLoad: Boolean? = null,
-    @SerialName("refresh_id") val refreshId: Int? = null,
-    @SerialName("is_replaceable") val isReplaceable: ReplaceAble? = null,
-    val pos: Vector3
+	val area: Int? = null,
+	val id: Long,
+	@SerialName("dynamic_load") val dynamicLoad: Boolean? = null,
+	@SerialName("refresh_id") val refreshId: Int? = null,
+	@SerialName("is_replaceable") val isReplaceable: ReplaceAble? = null,
+	val pos: Vector3
 )
 
 @Serializable
 data class ReplaceAble(
-    @SerialName("new_bin_only") val newBinOnly: Boolean,
-    val value: Boolean,
-    val version: Int
+	@SerialName("new_bin_only") val newBinOnly: Boolean,
+	val value: Boolean,
+	val version: Int
 )
 
 @Serializable
 data class Group(
-    @SerialName("gadgets") private val rawGadgets: JsonElement,
-    @SerialName("init_config") val initConfig: GroupInitConfig,
-    @SerialName("monsters") val rawMonsters: JsonElement,
-    val npcs: List<NPC>,
-    @SerialName("regions") private val rawRegions: JsonElement,
-    val suites: List<Suite>,
-    val variables: List<GroupVariable>
+	@SerialName("gadgets") private val rawGadgets: JsonElement,
+	@SerialName("init_config") val initConfig: GroupInitConfig,
+	@SerialName("monsters") val rawMonsters: JsonElement,
+	val npcs: List<NPC>,
+	@SerialName("regions") private val rawRegions: JsonElement,
+	val suites: List<Suite>,
+	val variables: List<GroupVariable>
 ) {
 
-    val gadgets: List<Gadget> by lazy {
-        decodeMultipleFormValue(rawGadgets)
-    }
+	val gadgets: List<Gadget> by lazy {
+		decodeMultipleFormValue(rawGadgets)
+	}
 
-    val regions: List<Region> by lazy {
-        decodeMultipleFormValue(rawRegions)
-    }
+	val regions: List<Region> by lazy {
+		decodeMultipleFormValue(rawRegions)
+	}
 
-    val monsters: List<Monster> by lazy {
-        decodeMultipleFormValue(rawMonsters)
-    }
+	val monsters: List<Monster> by lazy {
+		decodeMultipleFormValue(rawMonsters)
+	}
 
 }
 
 inline fun <reified T> decodeMultipleFormValue(jsonElement: JsonElement): List<T> {
-    val list = when(jsonElement) {
-        is JsonObject -> jsonElement.values.toList()
-        is JsonArray -> jsonElement.toList()
-        else -> error("mhy nmsl")
-    }
-    return list.map { jsonFormatter.decodeFromJsonElement(it) }
+	val list = when (jsonElement) {
+		is JsonObject -> jsonElement.values.toList()
+		is JsonArray -> jsonElement.toList()
+		else -> error("mhy nmsl")
+	}
+	return list.map { jsonFormatter.decodeFromJsonElement(it) }
 }
 
 @Serializable
 data class GroupVariable(
-    val configId: Int,
-    val name: String,
-    @SerialName("no_refresh") val noRefresh: Boolean,
-    val value: Int
+	val configId: Int,
+	val name: String,
+	@SerialName("no_refresh") val noRefresh: Boolean,
+	val value: Int
 )
 
 @Serializable
 data class Region(
-    @SerialName("config_id") val configId: Int,
-    val pos: Vector3,
-    val radius: Double? = null,
-    val shape: Int
+	@SerialName("config_id") val configId: Int,
+	val pos: Vector3,
+	val radius: Double? = null,
+	val shape: Int
 )
 
 @Serializable
 data class GroupInitConfig(
-    @SerialName("end_suite") val endSuite: Int,
-    @SerialName("rand_suite") val randSuite: Boolean,
-    val suite: Int
+	@SerialName("end_suite") val endSuite: Int,
+	@SerialName("rand_suite") val randSuite: Boolean,
+	val suite: Int
 )
 
 @Serializable
 data class Gadget(
-    @SerialName("config_id") val configId: Int,
-    @SerialName("gadget_id") val gadgetId: Int,
-    val level: Int,
-    val pos: Vector3,
-    val rot: Vector3,
-    @SerialName("drop_count") val dropCount: Int? = null,
-    @SerialName("explore") val explore: ExploreInfo? = null,
-    val isOneoff: Boolean? = null,
-    @SerialName("area_id") val areaId: Int? = null,
-    val persistent: Boolean? = null,
-    @SerialName("chest_drop_id") val chestDropId: Int? = null,
-    @SerialName("start_route") val startRoute: Boolean? = null,
-    @SerialName("is_use_point_array") val isUsePointArray: Boolean? = null,
-    @SerialName("owner") val owner: Int? = null,
-    @SerialName("point_type") val pointType: Int? = null,
+	@SerialName("config_id") val configId: Int,
+	@SerialName("gadget_id") val gadgetId: Int,
+	val level: Int,
+	val pos: Vector3,
+	val rot: Vector3,
+	@SerialName("drop_count") val dropCount: Int? = null,
+	@SerialName("explore") val explore: ExploreInfo? = null,
+	val isOneoff: Boolean? = null,
+	@SerialName("area_id") val areaId: Int? = null,
+	val persistent: Boolean? = null,
+	@SerialName("chest_drop_id") val chestDropId: Int? = null,
+	@SerialName("start_route") val startRoute: Boolean? = null,
+	@SerialName("is_use_point_array") val isUsePointArray: Boolean? = null,
+	@SerialName("owner") val owner: Int? = null,
+	@SerialName("point_type") val pointType: Int? = null,
 )
 
 @Serializable
 data class Monster(
-    val rot: Vector3,
-    val pos: Vector3,
-    @SerialName("config_id") val configId: Int,
-    val level: Int,
-    @SerialName("monster_id") val monsterId: Int,
+	val rot: Vector3,
+	val pos: Vector3,
+	@SerialName("config_id") val configId: Int,
+	val level: Int,
+	@SerialName("monster_id") val monsterId: Int,
 )
 
 @Serializable
 data class NPC(
-    val rot: Vector3,
-    val pos: Vector3,
-    @SerialName("config_id") val configId: Int,
-    @SerialName("npc_id") val npcId: Int,
-    @SerialName("area_id") val areaId: Int? = null,
-    val room: Int? = null
+	val rot: Vector3,
+	val pos: Vector3,
+	@SerialName("config_id") val configId: Int,
+	@SerialName("npc_id") val npcId: Int,
+	@SerialName("area_id") val areaId: Int? = null,
+	val room: Int? = null
 )
 
 @Serializable
 data class Suite(
-    val gadgets: List<Int>,
-    val monsters: List<Int>,
-    @SerialName("rand_weight") val randWeight: Int,
-    val npcs: List<Int>? = null,
-    val regions: List<Int>
+	val gadgets: List<Int>,
+	val monsters: List<Int>,
+	@SerialName("rand_weight") val randWeight: Int,
+	val npcs: List<Int>? = null,
+	val regions: List<Int>
 )
 
 @Serializable
 data class ExploreInfo(
-    val exp: Int,
-    val name: String
+	val exp: Int,
+	val name: String
 )
 
 @Serializable
 data class BlockRectangle(
-    val max: Vector2,
-    val min: Vector2
+	val max: Vector2,
+	val min: Vector2
 ) {
 
-    fun contains(pos: Vector2): Boolean {
-        return pos.x < max.x && pos.x > min.x && pos.z < max.z && pos.z > min.z
-    }
+	fun contains(pos: Vector2): Boolean {
+		return pos.x < max.x && pos.x > min.x && pos.z < max.z && pos.z > min.z
+	}
 
 }
 
 @Serializable
 data class Vector2(
-    val x: Float,
-    val z: Float
+	val x: Float,
+	val z: Float
 )
 
 @Serializable
 data class Vector3(
-    val x: Float,
-    val y: Float,
-    val z: Float
+	val x: Float,
+	val y: Float,
+	val z: Float
 )
--- src/main/java/emu/grasscutter/game/GenshinScene.java
+++ src/main/java/emu/grasscutter/game/GenshinScene.java
@@ -32,7 +32,7 @@ public class GenshinScene {
 	private final SceneData sceneData;
 	private final List<GenshinPlayer> players;
 	private final Int2ObjectMap<GenshinEntity> entities;
-	
+
 	private int time;
 	private ClimateType climate;
 	private int weather;
@@ -46,7 +46,7 @@ public class GenshinScene {
 		this.time = 8 * 60;
 		this.climate = ClimateType.CLIMATE_SUNNY;
 	}
-	
+
 	public int getId() {
 		return sceneData.getId();
 	}
@@ -54,11 +54,11 @@ public class GenshinScene {
 	public World getWorld() {
 		return world;
 	}
-	
+
 	public SceneData getSceneData() {
 		return this.sceneData;
 	}
-	
+
 	public SceneType getSceneType() {
 		return getSceneData().getSceneType();
 	}
@@ -66,7 +66,7 @@ public class GenshinScene {
 	public List<GenshinPlayer> getPlayers() {
 		return players;
 	}
-	
+
 	public int getPlayerCount() {
 		return this.getPlayers().size();
 	}
@@ -74,11 +74,11 @@ public class GenshinScene {
 	public Int2ObjectMap<GenshinEntity> getEntities() {
 		return entities;
 	}
-	
+
 	public GenshinEntity getEntityById(int id) {
 		return this.entities.get(id);
 	}
-	
+
 	public int getTime() {
 		return time;
 	}
@@ -86,7 +86,7 @@ public class GenshinScene {
 	public void changeTime(int time) {
 		this.time = time % 1440;
 	}
-	
+
 	public ClimateType getClimate() {
 		return climate;
 	}
@@ -106,45 +106,45 @@ public class GenshinScene {
 	public boolean isInScene(GenshinEntity entity) {
 		return this.entities.containsKey(entity.getId());
 	}
-	
+
 	public void addPlayer(GenshinPlayer player) {
 		// Check if player already in
 		if (getPlayers().contains(player)) {
 			return;
 		}
-		
+
 		// Remove player from prev scene
 		if (player.getScene() != null) {
 			player.getScene().removePlayer(player);
 		}
-			
+
 		// Add
 		getPlayers().add(player);
 		player.setSceneId(this.getId());
 		player.setScene(this);
-		
+
 		this.setupPlayerAvatars(player);
 	}
-	
+
 	public void removePlayer(GenshinPlayer player) {
 		// Remove player from scene
 		getPlayers().remove(player);
 		player.setScene(null);
-		
+
 		// Remove player avatars
 		this.removePlayerAvatars(player);
-		
+
 		// Remove player gadgets
 		for (EntityGadget gadget : player.getTeamManager().getGadgets()) {
 			this.removeEntity(gadget);
 		}
-		
+
 		// Deregister scene if not in use
 		if (this.getEntities().size() <= 0) {
 			this.getWorld().deregisterScene(this);
 		}
 	}
-	
+
 	private void setupPlayerAvatars(GenshinPlayer player) {
 		// Clear entities from old team
 		player.getTeamManager().getActiveTeam().clear();
@@ -155,13 +155,13 @@ public class GenshinScene {
 			EntityAvatar entity = new EntityAvatar(player.getScene(), player.getAvatars().getAvatarById(avatarId));
 			player.getTeamManager().getActiveTeam().add(entity);
 		}
-		
+
 		// Limit character index in case its out of bounds
 		if (player.getTeamManager().getCurrentCharacterIndex() >= player.getTeamManager().getActiveTeam().size() || player.getTeamManager().getCurrentCharacterIndex() < 0) {
 			player.getTeamManager().setCurrentCharacterIndex(player.getTeamManager().getCurrentCharacterIndex() - 1);
 		}
 	}
-	
+
 	private void removePlayerAvatars(GenshinPlayer player) {
 		Iterator<EntityAvatar> it = player.getTeamManager().getActiveTeam().iterator();
 		while (it.hasNext()) {
@@ -169,44 +169,44 @@ public class GenshinScene {
 			it.remove();
 		}
 	}
-	
+
 	public void spawnPlayer(GenshinPlayer player) {
 		if (this.isInScene(player.getTeamManager().getCurrentAvatarEntity())) {
 			return;
 		}
-		
+
 		if (player.getTeamManager().getCurrentAvatarEntity().getFightProperty(FightProperty.FIGHT_PROP_CUR_HP) <= 0f) {
 			player.getTeamManager().getCurrentAvatarEntity().setFightProperty(FightProperty.FIGHT_PROP_CUR_HP, 1f);
 		}
-		
+
 		this.addEntity(player.getTeamManager().getCurrentAvatarEntity());
 	}
-	
+
 	private void addEntityDirectly(GenshinEntity entity) {
 		getEntities().put(entity.getId(), entity);
 	}
-	
+
 	public synchronized void addEntity(GenshinEntity entity) {
 		this.addEntityDirectly(entity);
 		this.broadcastPacket(new PacketSceneEntityAppearNotify(entity));
 	}
-	
+
 	public synchronized void addEntities(Collection<GenshinEntity> entities) {
 		for (GenshinEntity entity : entities) {
 			this.addEntityDirectly(entity);
 		}
-		
+
 		this.broadcastPacket(new PacketSceneEntityAppearNotify(entities, VisionType.VisionBorn));
 	}
-	
+
 	private GenshinEntity removeEntityDirectly(GenshinEntity entity) {
 		return getEntities().remove(entity.getId());
 	}
-	
+
 	public void removeEntity(GenshinEntity entity) {
 		this.removeEntity(entity, VisionType.VisionDie);
 	}
-	
+
 	public synchronized void removeEntity(GenshinEntity entity, VisionType visionType) {
 		GenshinEntity removed = this.removeEntityDirectly(entity);
 		if (removed != null) {
@@ -220,18 +220,18 @@ public class GenshinScene {
 		this.broadcastPacket(new PacketSceneEntityDisappearNotify(oldEntity, VisionType.VisionReplace));
 		this.broadcastPacket(new PacketSceneEntityAppearNotify(newEntity, VisionType.VisionReplace, oldEntity.getId()));
 	}
-	
+
 	public void showOtherEntities(GenshinPlayer player) {
 		List<GenshinEntity> entities = new LinkedList<>();
 		GenshinEntity currentEntity = player.getTeamManager().getCurrentAvatarEntity();
-		
+
 		for (GenshinEntity entity : this.getEntities().values()) {
 			if (entity == currentEntity) {
 				continue;
 			}
 			entities.add(entity);
 		}
-		
+
 		player.sendPacket(new PacketSceneEntityAppearNotify(entities, VisionType.VisionMeet));
 	}
 
@@ -287,114 +287,114 @@ public class GenshinScene {
 			player.sendPacket(new PacketSceneEntityDisappearNotify(disappear, VisionType.VisionTransport));
 			player.sendPacket(new PacketSceneEntityAppearNotify(appear, VisionType.VisionMeet));
 		} else {
-			Grasscutter.getLogger().error("Player scene is wrong, could not get any scene groups from player "+ player.getNickname());
+			Grasscutter.getLogger().error("Player scene is wrong, could not get any scene groups from player " + player.getNickname());
 		}
 	}
 
-	
+
 	public void handleAttack(AttackResult result) {
 		//GenshinEntity attacker = getEntityById(result.getAttackerId());
 		GenshinEntity target = getEntityById(result.getDefenseId());
-		
+
 		if (target == null) {
 			return;
 		}
-		
+
 		// Godmode check
 		if (target instanceof EntityAvatar) {
 			if (((EntityAvatar) target).getPlayer().inGodmode()) {
 				return;
 			}
 		}
-		
+
 		// Lose hp
 		target.addFightProperty(FightProperty.FIGHT_PROP_CUR_HP, -result.getDamage());
-		
+
 		// Check if dead
 		boolean isDead = false;
 		if (target.getFightProperty(FightProperty.FIGHT_PROP_CUR_HP) <= 0f) {
 			target.setFightProperty(FightProperty.FIGHT_PROP_CUR_HP, 0f);
 			isDead = true;
 		}
-		
+
 		// Packets
 		this.broadcastPacket(new PacketEntityFightPropUpdateNotify(target, FightProperty.FIGHT_PROP_CUR_HP));
-		
+
 		// Check if dead
 		if (isDead) {
 			this.killEntity(target, result.getAttackerId());
 		}
 	}
-	
+
 	public void killEntity(GenshinEntity target, int attackerId) {
 		// Packet
 		this.broadcastPacket(new PacketLifeStateChangeNotify(attackerId, target, LifeState.LIFE_DEAD));
 		this.removeEntity(target);
-		
+
 		// Death event
 		target.onDeath(attackerId);
 	}
-	
+
 	// Gadgets
-	
+
 	public void onPlayerCreateGadget(EntityClientGadget gadget) {
 		// Directly add
 		this.addEntityDirectly(gadget);
-		
+
 		// Add to owner's gadget list
 		gadget.getOwner().getTeamManager().getGadgets().add(gadget);
-		
+
 		// Optimization
 		if (this.getPlayerCount() == 1 && this.getPlayers().get(0) == gadget.getOwner()) {
 			return;
 		}
-		
+
 		this.broadcastPacketToOthers(gadget.getOwner(), new PacketSceneEntityAppearNotify(gadget));
 	}
-	
+
 	public void onPlayerDestroyGadget(int entityId) {
 		GenshinEntity entity = getEntities().get(entityId);
-		
+
 		if (entity == null || !(entity instanceof EntityClientGadget)) {
 			return;
 		}
-		
+
 		// Get and remove entity
 		EntityClientGadget gadget = (EntityClientGadget) entity;
 		this.removeEntityDirectly(gadget);
-		
+
 		// Remove from owner's gadget list
 		gadget.getOwner().getTeamManager().getGadgets().remove(gadget);
-		
+
 		// Optimization
 		if (this.getPlayerCount() == 1 && this.getPlayers().get(0) == gadget.getOwner()) {
 			return;
 		}
-		
+
 		this.broadcastPacketToOthers(gadget.getOwner(), new PacketSceneEntityDisappearNotify(gadget, VisionType.VisionDie));
 	}
 
 	// Broadcasting
-	
+
 	public void broadcastPacket(GenshinPacket packet) {
-    	// Send to all players - might have to check if player has been sent data packets
-    	for (GenshinPlayer player : this.getPlayers()) {
-    		player.getSession().send(packet);
-    	}
+		// Send to all players - might have to check if player has been sent data packets
+		for (GenshinPlayer player : this.getPlayers()) {
+			player.getSession().send(packet);
+		}
 	}
-	
+
 	public void broadcastPacketToOthers(GenshinPlayer excludedPlayer, GenshinPacket packet) {
 		// Optimization
 		if (this.getPlayerCount() == 1 && this.getPlayers().get(0) == excludedPlayer) {
 			return;
 		}
-    	// Send to all players - might have to check if player has been sent data packets
-    	for (GenshinPlayer player : this.getPlayers()) {
-    		if (player == excludedPlayer) {
-    			continue;
-    		}
-    		// Send
-    		player.getSession().send(packet);
-    	}
+		// Send to all players - might have to check if player has been sent data packets
+		for (GenshinPlayer player : this.getPlayers()) {
+			if (player == excludedPlayer) {
+				continue;
+			}
+			// Send
+			player.getSession().send(packet);
+		}
 	}
 }
