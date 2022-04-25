122568ad2767a08fc81bf3550c425462343b6528
Asnxthaony <asnxthaony@outlook.com>
2022-04-25 13:46:16 +0800

    fix SetPlayerBirthdayRsp

--- src/main/java/emu/grasscutter/server/packet/send/PacketSetPlayerBirthdayRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketSetPlayerBirthdayRsp.java
@@ -1,18 +1,16 @@
 package emu.grasscutter.server.packet.send;
 
-import emu.grasscutter.Grasscutter;
 import emu.grasscutter.game.GenshinPlayer;
 import emu.grasscutter.net.packet.GenshinPacket;
 import emu.grasscutter.net.packet.PacketOpcodes;
 import emu.grasscutter.net.proto.SetPlayerBirthdayRspOuterClass.SetPlayerBirthdayRsp;
-import emu.grasscutter.net.proto.BirthdayOuterClass.Birthday;
 
 public class PacketSetPlayerBirthdayRsp extends GenshinPacket {
     public PacketSetPlayerBirthdayRsp(GenshinPlayer player) {
         super(PacketOpcodes.SetPlayerBirthdayRsp);
 
         SetPlayerBirthdayRsp proto = SetPlayerBirthdayRsp.newBuilder()
-                .setBirth(player.getBirthday().toProto())
+                .setBirthday(player.getBirthday().toProto())
                 .build();
 
         this.setData(proto);
