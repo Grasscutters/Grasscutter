e8b34b6179a80de1ecc8b179f324c0ccf02324b6
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-04-27 07:14:02 +0800

    comments del

--- src/main/java/emu/grasscutter/task/Task.java
+++ src/main/java/emu/grasscutter/task/Task.java
@@ -6,17 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 
 /*
 * So what is cron expression?
-* Check this: https://en.wikipedia.org/wiki/Cron
-# ┌───────────── minute (0 - 59)
-# │ ┌───────────── hour (0 - 23)
-# │ │ ┌───────────── day of the month (1 - 31)
-# │ │ │ ┌───────────── month (1 - 12)
-# │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday;
-# │ │ │ │ │                                   7 is also Sunday on some systems)
-# │ │ │ │ │
-# │ │ │ │ │
-# * * * * *
-* */
+* second minute hour day month week year
+*/
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Task {
     String taskName() default "NO_NAME";
