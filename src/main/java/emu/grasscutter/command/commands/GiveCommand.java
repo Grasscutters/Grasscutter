db1b94aab0869f04be7d2ea9fd4e5abe1cd08e24
Miqdad Farcha <miqdad.farcha@gmail.com>
2022-04-27 12:16:08 +0700

    add all item command

--- src/main/java/emu/grasscutter/command/commands/GiveCommand.java
+++ src/main/java/emu/grasscutter/command/commands/GiveCommand.java
@@ -10,8 +10,10 @@ import emu.grasscutter.game.inventory.GenshinItem;
 import emu.grasscutter.game.props.ActionReason;
 import emu.grasscutter.server.packet.send.PacketItemAddHintNotify;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Command(label = "give", usage = "give [player] <itemId|itemName> [amount]",
         description = "Gives an item to you or the specified player", aliases = {"g", "item", "giveitem"}, permission = "player.give")
@@ -19,7 +21,8 @@ public final class GiveCommand implements CommandHandler {
 
     @Override
     public void execute(GenshinPlayer sender, List<String> args) {
-        int target, item, amount = 1;
+        int target, amount = 1;
+        String item;
 
         if (sender == null && args.size() < 2) {
             CommandHandler.sendMessage(null, "Usage: give <player> <itemId|itemName> [amount]");
@@ -31,48 +34,30 @@ public final class GiveCommand implements CommandHandler {
                 CommandHandler.sendMessage(sender, "Usage: give [player] <itemId|itemName> [amount]");
                 return;
             case 1: // <itemId|itemName>
-                try {
-                    item = Integer.parseInt(args.get(0));
-                    target = sender.getUid();
-                } catch (NumberFormatException ignored) {
-                    // TODO: Parse from item name using GM Handbook.
-                    CommandHandler.sendMessage(sender, "Invalid item id.");
-                    return;
-                }
+                item = args.get(0);
+                target = sender.getUid();
                 break;
             case 2: // <itemId|itemName> [amount] | [player] <itemId|itemName>
-                try {
-                    target = Integer.parseInt(args.get(0));
+                target = Integer.parseInt(args.get(0));
 
-                    if (Grasscutter.getGameServer().getPlayerByUid(target) == null && sender != null) {
-                        target = sender.getUid();
-                        item = Integer.parseInt(args.get(0));
-                        amount = Integer.parseInt(args.get(1));
-                    } else {
-                        item = Integer.parseInt(args.get(1));
-                    }
-                } catch (NumberFormatException ignored) {
-                    // TODO: Parse from item name using GM Handbook.
-                    CommandHandler.sendMessage(sender, "Invalid item or player ID.");
-                    return;
+                if (Grasscutter.getGameServer().getPlayerByUid(target) == null && sender != null) {
+                    target = sender.getUid();
+                    item = args.get(0);
+                    amount = Integer.parseInt(args.get(1));
+                } else {
+                    item = args.get(1);
                 }
                 break;
             case 3: // [player] <itemId|itemName> [amount]
-                try {
-                    target = Integer.parseInt(args.get(0));
+                target = Integer.parseInt(args.get(0));
 
-                    if (Grasscutter.getGameServer().getPlayerByUid(target) == null) {
-                        CommandHandler.sendMessage(sender, "Invalid player ID.");
-                        return;
-                    }
-
-                    item = Integer.parseInt(args.get(1));
-                    amount = Integer.parseInt(args.get(2));
-                } catch (NumberFormatException ignored) {
-                    // TODO: Parse from item name using GM Handbook.
-                    CommandHandler.sendMessage(sender, "Invalid item or player ID.");
+                if (Grasscutter.getGameServer().getPlayerByUid(target) == null) {
+                    CommandHandler.sendMessage(sender, "Invalid player ID.");
                     return;
                 }
+
+                item = args.get(1);
+                amount = Integer.parseInt(args.get(2));
                 break;
         }
 
@@ -83,13 +68,23 @@ public final class GiveCommand implements CommandHandler {
             return;
         }
 
-        ItemData itemData = GenshinData.getItemDataMap().get(item);
-        if (itemData == null) {
-            CommandHandler.sendMessage(sender, "Invalid item id.");
-            return;
+        if (item.equals("all")) {
+            List<ItemData> items = new ArrayList<>(GenshinData.getItemDataMap().values());
+            this.item(targetPlayer, items, amount);
+        } else {
+            try {
+                ItemData itemData = GenshinData.getItemDataMap().get(Integer.parseInt(item));
+                if (itemData == null) {
+                    CommandHandler.sendMessage(sender, "Invalid item id.");
+                    return;
+                }
+                this.item(targetPlayer, itemData, amount);
+            } catch (NumberFormatException e) {
+                CommandHandler.sendMessage(sender, "Invalid item id.");
+                return;
+            }
         }
 
-        this.item(targetPlayer, itemData, amount);
 
         CommandHandler.sendMessage(sender, String.format("Given %s of %s to %s.", amount, item, target));
     }
@@ -108,6 +103,19 @@ public final class GiveCommand implements CommandHandler {
             player.getInventory().addItem(genshinItem);
             player.sendPacket(new PacketItemAddHintNotify(genshinItem, ActionReason.SubfieldDrop));
         }
+    }
+
+    private void item(GenshinPlayer player, List<ItemData> itemDataList, int amount) {
+        List<GenshinItem> genshinItems = new LinkedList<GenshinItem>();
+        for (ItemData itemData : itemDataList) {
+            // Ignore give weapon to character
+            if (itemData.isEquip()) continue;
+            GenshinItem genshinItem = new GenshinItem(itemData);
+            genshinItem.setCount(amount);
+            player.getInventory().addItem(genshinItem);
+            genshinItems.add(genshinItem);
+        }
+        player.sendPacket(new PacketItemAddHintNotify(genshinItems, ActionReason.SubfieldDrop));
     }
 }
 
