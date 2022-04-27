5b351b955cb3b304cc4995dcf354357eecd31074
KingRainbow44 <kobedo11@gmail.com>
2022-04-26 21:24:37 -0400

    Add `get` methods to the server hook

--- src/main/java/emu/grasscutter/plugin/api/ServerHook.java
+++ src/main/java/emu/grasscutter/plugin/api/ServerHook.java
@@ -1,5 +1,7 @@
 package emu.grasscutter.plugin.api;
 
+import emu.grasscutter.command.Command;
+import emu.grasscutter.command.CommandHandler;
 import emu.grasscutter.game.GenshinPlayer;
 import emu.grasscutter.server.dispatch.DispatchServer;
 import emu.grasscutter.server.game.GameServer;
@@ -36,10 +38,36 @@ public final class ServerHook {
     }
 
     /**
+     * @return The game server.
+     */
+    public GameServer getGameServer() {
+        return this.gameServer;
+    }
+
+    /**
+     * @return The dispatch server.
+     */
+    public DispatchServer getDispatchServer() {
+        return this.dispatchServer;
+    }
+    
+    /**
      * Gets all online players.
      * @return Players connected to the server.
      */
     public List<GenshinPlayer> getOnlinePlayers() {
         return new LinkedList<>(this.gameServer.getPlayers().values());
+    }
+
+    /**
+     * Registers a command to the {@link emu.grasscutter.command.CommandMap}.
+     * @param handler The command handler.
+     */
+    public void registerCommand(CommandHandler handler) {
+        Class<? extends CommandHandler> clazz = handler.getClass();
+        if(!clazz.isAnnotationPresent(Command.class))
+            throw new IllegalArgumentException("Command handler must be annotated with @Command.");
+        Command commandData = clazz.getAnnotation(Command.class);
+        this.gameServer.getCommandMap().registerCommand(commandData.label(), handler);
     }
 }
