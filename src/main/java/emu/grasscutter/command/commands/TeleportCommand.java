74ad63573a97cdf9447af3422ade011b2da45afb
xtaodada <xtao@xtaolink.cn>
2022-04-29 17:53:42 +0800

    Support Teleport from console

--- src/main/java/emu/grasscutter/command/commands/TeleportCommand.java
+++ src/main/java/emu/grasscutter/command/commands/TeleportCommand.java
@@ -1,5 +1,6 @@
 package emu.grasscutter.command.commands;
 
+import emu.grasscutter.Grasscutter;
 import emu.grasscutter.command.Command;
 import emu.grasscutter.command.CommandHandler;
 import emu.grasscutter.game.player.Player;
@@ -7,21 +8,39 @@ import emu.grasscutter.utils.Position;
 
 import java.util.List;
 
-@Command(label = "teleport", usage = "teleport <x> <y> <z>", aliases = {"tp"},
+@Command(label = "teleport", usage = "teleport [@player id] <x> <y> <z> [scene id]", aliases = {"tp"},
         description = "Change the player's position.", permission = "player.teleport")
 public final class TeleportCommand implements CommandHandler {
 
     @Override
     public void execute(Player sender, List<String> args) {
-        if (sender == null) {
-            CommandHandler.sendMessage(null, "Run this command in-game.");
+        int target;
+        if (args.size() < (sender == null ? 4 : 3)) {
+            CommandHandler.sendMessage(sender, sender == null ? "Usage: /tp @<player id> <x> <y> <z> [scene id]" :
+                    "Usage: /tp [@<player id>] <x> <y> <z> [scene id]");
             return;
+        }
+        if (args.get(0).startsWith("@")) {
+            try {
+                target = Integer.parseInt(args.get(0).substring(1));
+            } catch (NumberFormatException e) {
+                CommandHandler.sendMessage(sender, "Invalid player id.");
+                return;
+            }
+        } else {
+            if (sender == null) {
+                CommandHandler.sendMessage(null, "You must specify a player id.");
+                return;
+            }
+            target = sender.getUid();
         }
 
-        if (args.size() < 3){
-            CommandHandler.sendMessage(sender, "Usage: /tp <x> <y> <z> [scene id]");
+        Player targetPlayer = Grasscutter.getGameServer().getPlayerByUid(target);
+        if (targetPlayer == null) {
+            CommandHandler.sendMessage(sender, "Player not found or offline.");
             return;
         }
+        args = args.subList(args.get(0).startsWith("@") ? 1 : 0, args.size());
 
         try {
             float x = 0f;
@@ -29,39 +48,41 @@ public final class TeleportCommand implements CommandHandler {
             float z = 0f;
             if (args.get(0).contains("~")) {
                 if (args.get(0).equals("~")) {
-                    x = sender.getPos().getX();
+                    x = targetPlayer.getPos().getX();
                 } else {
-                    x = Float.parseFloat(args.get(0).replace("~", "")) + sender.getPos().getX();
+                    x = Float.parseFloat(args.get(0).replace("~", "")) + targetPlayer.getPos().getX();
                 }
             } else {
                 x = Float.parseFloat(args.get(0));
             }
             if (args.get(1).contains("~")) {
                 if (args.get(1).equals("~")) {
-                    y = sender.getPos().getY();
+                    y = targetPlayer.getPos().getY();
                 } else {
-                    y = Float.parseFloat(args.get(1).replace("~", "")) + sender.getPos().getY();
+                    y = Float.parseFloat(args.get(1).replace("~", "")) + targetPlayer.getPos().getY();
                 }
             } else {
                 y = Float.parseFloat(args.get(1));
             }
             if (args.get(2).contains("~")) {
                 if (args.get(2).equals("~")) {
-                    z = sender.getPos().getZ();
+                    z = targetPlayer.getPos().getZ();
                 } else {
-                    z = Float.parseFloat(args.get(2).replace("~", "")) + sender.getPos().getZ();
+                    z = Float.parseFloat(args.get(2).replace("~", "")) + targetPlayer.getPos().getZ();
                 }
             } else {
                 z = Float.parseFloat(args.get(2));
             }
-            int sceneId = sender.getSceneId();
+            int sceneId = targetPlayer.getSceneId();
             if (args.size() == 4){
                 sceneId = Integer.parseInt(args.get(3));
             }
-            Position target = new Position(x, y, z);
-            boolean result = sender.getWorld().transferPlayerToScene(sender, sceneId, target);
+            Position target_pos = new Position(x, y, z);
+            boolean result = targetPlayer.getWorld().transferPlayerToScene(targetPlayer, sceneId, target_pos);
             if (!result) {
                 CommandHandler.sendMessage(sender, "Invalid position.");
+            } else {
+                CommandHandler.sendMessage(sender, "Teleported " + targetPlayer.getNickname() + " to " + x + "," + y + "," + z + " in scene " + sceneId);
             }
         } catch (NumberFormatException ignored) {
             CommandHandler.sendMessage(sender, "Invalid position.");
