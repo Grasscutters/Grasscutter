6d969064843dd934f6ece36bab6681a2376ff002
xtaodada <xtao@xtaolink.cn>
2022-05-02 04:15:32 +0800

    Show shopmall

--- src/main/java/emu/grasscutter/server/packet/send/PacketGetShopmallDataRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGetShopmallDataRsp.java
@@ -4,13 +4,19 @@ import emu.grasscutter.net.packet.BasePacket;
 import emu.grasscutter.net.packet.PacketOpcodes;
 import emu.grasscutter.net.proto.GetShopmallDataRspOuterClass.GetShopmallDataRsp;
 
+import java.util.List;
+
 public class PacketGetShopmallDataRsp extends BasePacket {
 	
 	public PacketGetShopmallDataRsp() {
 		super(PacketOpcodes.GetShopmallDataRsp);
 
-		GetShopmallDataRsp proto = GetShopmallDataRsp.newBuilder().build();
-		
+		List<Integer> shop_malls = List.of(900, 1052, 902, 1001, 903);
+
+		GetShopmallDataRsp proto = GetShopmallDataRsp.newBuilder()
+				.addAllShopTypeList(shop_malls)
+				.build();
+
 		this.setData(proto);
 	}
 }
