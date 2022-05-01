6c844291fbf324c99538f501fbc290ddde961422
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-05-02 04:56:56 +0800

    tes

--- src/main/java/emu/grasscutter/command/commands/GiveAllCommand.java
+++ src/main/java/emu/grasscutter/command/commands/GiveAllCommand.java
@@ -14,99 +14,64 @@ import emu.grasscutter.game.player.Player;
 
 import java.util.*;
 
-@Command(label = "giveall", usage = "giveall [player] [amount]",
-        description = "Gives all items", aliases = {"givea"}, permission = "player.giveall", threading = true)
+@Command(label = "giveall", usage = "giveall", description = "Gives all items", aliases = {
+        "givea" }, permission = "player.giveall", threading = true)
 public final class GiveAllCommand implements CommandHandler {
 
     @Override
     public void execute(Player sender, List<String> args) {
-        int target, amount = 1000;
 
-        switch (args.size()) {
-            case 0: // *no args*
-                if (sender == null) {
-                    CommandHandler.sendMessage(null, "This usage can only be run in-game");
-                    return;
-                }
-                target = sender.getUid();
-                break;
+        // TODO: need configuration stuff
+        int target = sender.getUid();
+        int amount = 1000;
 
-            case 1: // [player]
-                try {
-                    target = Integer.parseInt(args.get(0));
-                    if (Grasscutter.getGameServer().getPlayerByUid(target) == null) {
-                        CommandHandler.sendMessage(sender, "Invalid player ID.");
-                        return;
-                    }
-                }catch (NumberFormatException ignored){
-                    CommandHandler.sendMessage(sender, "Invalid player ID.");
-                    return;
-                }
-                break;
-
-            case 2: // [player] [amount]
-                try {
-                    target = Integer.parseInt(args.get(0));
-                    if (Grasscutter.getGameServer().getPlayerByUid(target) == null) {
-                        target = sender.getUid();
-                        amount = Integer.parseInt(args.get(0));
-                    } else {
-                        amount = Integer.parseInt(args.get(1));
-                    }
-                } catch (NumberFormatException ignored) {
-                    CommandHandler.sendMessage(sender, "Invalid amount or player ID.");
-                    return;
-                }
-                break;
-
-            default: // invalid
-                CommandHandler.sendMessage(null, "Usage: giveall [player] [amount]");
-                return;
-        }
-
+        // Check Uid
         Player targetPlayer = Grasscutter.getGameServer().getPlayerByUid(target);
         if (targetPlayer == null) {
             CommandHandler.sendMessage(sender, "Player not found.");
             return;
         }
-        
-        this.giveAllItems(targetPlayer, amount);
-        CommandHandler.sendMessage(sender, "Giving all items done");
-
+        // Check Username
         Account account = Grasscutter.getGameServer().getAccountByName(sender.getAccount().getUsername());
         if (account == null) {
-            CommandHandler.sendMessage(sender, "Account not found.");
+            CommandHandler.sendMessage(sender, "Account not found?");
             return;
         }
 
+        // Remove permission
         if (account.removePermission("player.giveall")) {
-            CommandHandler.sendMessage(sender, "Permission removed, can only be used once");
-        } else CommandHandler.sendMessage(sender, "They don't have this permission!");
+            this.giveAllItems(targetPlayer, amount);
+            CommandHandler.sendMessage(sender, "Giving all items done, Permission removed, can only be used once.");
+        } else
+            CommandHandler.sendMessage(sender, "They don't have this permission!");
+
     }
 
     public void giveAllItems(Player player, int amount) {
         CommandHandler.sendMessage(player, "Giving all items...");
 
-        for (AvatarData avatarData: GameData.getAvatarDataMap().values()) {
-            //Exclude test avatar
-            if (isTestAvatar(avatarData.getId())) continue;
+        for (AvatarData avatarData : GameData.getAvatarDataMap().values()) {
+            // Exclude test avatar
+            if (isTestAvatar(avatarData.getId()))
+                continue;
 
             Avatar avatar = new Avatar(avatarData);
             avatar.setLevel(90);
             avatar.setPromoteLevel(6);
-            for(int i = 1;i <= 6;++i){
-                avatar.getTalentIdList().add((avatar.getAvatarId()-10000000)*10+i);
+            for (int i = 1; i <= 6; ++i) {
+                avatar.getTalentIdList().add((avatar.getAvatarId() - 10000000) * 10 + i);
             }
             // This will handle stats and talents
             avatar.recalcStats();
             player.addAvatar(avatar);
         }
 
-        //some test items
+        // some test items
         List<GameItem> itemList = new ArrayList<>();
-        for (ItemData itemdata: GameData.getItemDataMap().values()) {
-            //Exclude test item
-            if (isTestItem(itemdata.getId())) continue;
+        for (ItemData itemdata : GameData.getItemDataMap().values()) {
+            // Exclude test item
+            if (isTestItem(itemdata.getId()))
+                continue;
 
             if (itemdata.isEquip()) {
                 if (itemdata.getItemType() == ItemType.ITEM_WEAPON) {
@@ -118,8 +83,7 @@ public final class GiveAllCommand implements CommandHandler {
                         itemList.add(item);
                     }
                 }
-            }
-            else {
+            } else {
                 GameItem item = new GameItem(itemdata);
                 item.setCount(amount);
                 itemList.add(item);
@@ -135,8 +99,7 @@ public final class GiveAllCommand implements CommandHandler {
                 player.getInventory().addItems(itemList.subList(i * number + offset, (i + 1) * number + offset + 1));
                 --remainder;
                 ++offset;
-            }
-            else {
+            } else {
                 player.getInventory().addItems(itemList.subList(i * number + offset, (i + 1) * number + offset));
             }
         }
@@ -147,7 +110,7 @@ public final class GiveAllCommand implements CommandHandler {
     }
 
     public boolean isTestItem(int itemId) {
-        for (Range range: testItemRanges) {
+        for (Range range : testItemRanges) {
             if (range.check(itemId)) {
                 return true;
             }
@@ -160,12 +123,12 @@ public final class GiveAllCommand implements CommandHandler {
         private final int min, max;
 
         public Range(int min, int max) {
-            if(min > max){
+            if (min > max) {
                 min ^= max;
                 max ^= min;
                 min ^= max;
             }
-            
+
             this.min = min;
             this.max = max;
         }
@@ -194,14 +157,13 @@ public final class GiveAllCommand implements CommandHandler {
             new Range(220050, 221016),
     };
     private static final Integer[] testItemsIds = new Integer[] {
-            210, 211, 314, 315, 317, 1005, 1007, 1105, 1107, 1201, 1202,10366,
+            210, 211, 314, 315, 317, 1005, 1007, 1105, 1107, 1201, 1202, 10366,
             101212, 11411, 11506, 11507, 11508, 12505, 12506, 12508, 12509, 13503,
             13506, 14411, 14503, 14505, 14508, 15411, 15504, 15505, 15506, 15508,
-            20001, 10002, 10003, 10004, 10005, 10006, 10008,100231,100232,100431,
-            101689,105001,105004, 106000,106001,108000,110000
+            20001, 10002, 10003, 10004, 10005, 10006, 10008, 100231, 100232, 100431,
+            101689, 105001, 105004, 106000, 106001, 108000, 110000
     };
 
     private static final Collection<Integer> testItemsList = Arrays.asList(testItemsIds);
 
 }
-
