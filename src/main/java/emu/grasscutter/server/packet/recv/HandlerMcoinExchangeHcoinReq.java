2a0757aa7a5977159fd0341df7d2d0e315cb5333
Kinesis <CCasusensa@users.noreply.github.com>
2022-05-01 16:24:23 +0800

    fix McoinExchangeHcoinRsp packet structure

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerMcoinExchangeHcoinReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerMcoinExchangeHcoinReq.java
@@ -4,6 +4,7 @@ import emu.grasscutter.net.packet.Opcodes;
 import emu.grasscutter.net.packet.PacketHandler;
 import emu.grasscutter.net.packet.PacketOpcodes;
 import emu.grasscutter.net.proto.McoinExchangeHcoinReqOuterClass;
+import emu.grasscutter.net.proto.RetcodeOuterClass;
 import emu.grasscutter.server.game.GameSession;
 import emu.grasscutter.server.packet.send.PacketMcoinExchangeHcoinRsp;
 
@@ -15,13 +16,13 @@ public class HandlerMcoinExchangeHcoinReq extends PacketHandler {
         McoinExchangeHcoinReqOuterClass.McoinExchangeHcoinReq exchangeReq = McoinExchangeHcoinReqOuterClass.McoinExchangeHcoinReq.parseFrom(payload);
         
         if (session.getPlayer().getCrystals() < exchangeReq.getMCoinNum() && exchangeReq.getMCoinNum() == exchangeReq.getHCoinNum()) {
+            session.send(new PacketMcoinExchangeHcoinRsp(RetcodeOuterClass.Retcode.RET_UNKNOWN_ERROR_VALUE));
             return;
         }
         
         session.getPlayer().setCrystals(session.getPlayer().getCrystals() - exchangeReq.getMCoinNum());
         session.getPlayer().setPrimogems(session.getPlayer().getPrimogems() + exchangeReq.getHCoinNum());
         session.getPlayer().save();
-        
-        session.send(new PacketMcoinExchangeHcoinRsp(session.getPlayer().getCrystals(), session.getPlayer().getPrimogems()));
+        session.send(new PacketMcoinExchangeHcoinRsp(RetcodeOuterClass.Retcode.RET_SUCC_VALUE));
     }
 }
