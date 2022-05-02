f7b8d9b45fd82c00716420fd276bca47a36eff73
4Benj_ <Benjamin7006@gmail.com>
2022-05-02 18:47:29 +0800

    Removed debug message and fixed NullPtrException (#434)
    * Removed a debug message from Auth Handler changes
    
    Kill me
    
    * Also forgot to use the getter :/

--- src/main/java/emu/grasscutter/server/dispatch/DispatchServer.java
+++ src/main/java/emu/grasscutter/server/dispatch/DispatchServer.java
@@ -309,7 +309,7 @@ public final class DispatchServer {
 			}
 			Grasscutter.getLogger().info(String.format("[Dispatch] Client %s is trying to log in", req.ip()));
 
-			res.send(authHandler.handleGameLogin(req, requestData));
+			res.send(this.getAuthHandler().handleGameLogin(req, requestData));
 		});
 
 		// Login via token
@@ -491,8 +491,6 @@ public final class DispatchServer {
 		if(authHandler == null) {
 			return new DefaultAuthenticationHandler();
 		}
-		Grasscutter.getLogger().info(authHandler.getClass().getName());
-
 		return authHandler;
 	}
 
