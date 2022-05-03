052e0d1a562d4a7b113e034817d6f3adc344a0d4
方块君 <fkj@fkj233.cn>
2022-05-03 14:10:56 +0800

    Add multilingual support

--- src/main/java/emu/grasscutter/command/commands/PositionCommand.java
+++ src/main/java/emu/grasscutter/command/commands/PositionCommand.java
@@ -1,5 +1,6 @@
 package emu.grasscutter.command.commands;
 
+import emu.grasscutter.Grasscutter;
 import emu.grasscutter.command.Command;
 import emu.grasscutter.command.CommandHandler;
 import emu.grasscutter.game.player.Player;
@@ -13,11 +14,11 @@ public final class PositionCommand implements CommandHandler {
     @Override
     public void execute(Player sender, List<String> args) {
         if (sender == null) {
-            CommandHandler.sendMessage(null, "Run this command in-game.");
+            CommandHandler.sendMessage(null, Grasscutter.getLanguage().Run_this_command_in_game);
             return;
         }
 
-        sender.dropMessage(String.format("Coord: %.3f, %.3f, %.3f\nScene id: %d",
+        sender.dropMessage(String.format(Grasscutter.getLanguage().Position_message,
                 sender.getPos().getX(), sender.getPos().getY(), sender.getPos().getZ(), sender.getSceneId()));
     }
 }
