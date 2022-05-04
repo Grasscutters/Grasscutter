e7276a75d311727f8978022da2a83db5296aa8b3
Melledy <52122272+Melledy@users.noreply.github.com>
2022-05-03 23:13:42 -0700

    Fixed dungeon challenge scoreboard and implement dungeon drops
    Also fixed a few dungeon script handlers

--- src/main/java/emu/grasscutter/data/GameData.java
+++ src/main/java/emu/grasscutter/data/GameData.java
@@ -66,7 +66,8 @@ public class GameData {
 	private static final Int2ObjectMap<DailyDungeonData> dailyDungeonDataMap = new Int2ObjectOpenHashMap<>();
 	private static final Int2ObjectMap<DungeonData> dungeonDataMap = new Int2ObjectOpenHashMap<>();
 	private static final Int2ObjectMap<ShopGoodsData> shopGoodsDataMap = new Int2ObjectOpenHashMap<>();
-
+	private static final Int2ObjectMap<RewardPreviewData> rewardPreviewDataMap = new Int2ObjectOpenHashMap<>();
+	
 	// Cache
 	private static Map<Integer, List<Integer>> fetters = new HashMap<>();
 	private static Map<Integer, List<ShopGoodsData>> shopGoods = new HashMap<>();
@@ -298,6 +299,13 @@ public class GameData {
 		}
 
 		return shopGoods;
+	}
+
+	/**
+	 * @return the rewardpreviewdatamap
+	 */
+	public static Int2ObjectMap<RewardPreviewData> getRewardPreviewDataMap() {
+		return rewardPreviewDataMap;
 	}
 
 	public static IntList getScenePointIdList() {
--- src/main/java/emu/grasscutter/game/dungeons/DungeonChallenge.java
+++ src/main/java/emu/grasscutter/game/dungeons/DungeonChallenge.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import emu.grasscutter.data.GameData;
+import emu.grasscutter.data.common.ItemParamData;
+import emu.grasscutter.data.def.DungeonData;
 import emu.grasscutter.data.def.MonsterData;
 import emu.grasscutter.game.entity.EntityMonster;
 import emu.grasscutter.game.entity.GameEntity;
+import emu.grasscutter.game.inventory.GameItem;
+import emu.grasscutter.game.player.Player;
+import emu.grasscutter.game.props.ActionReason;
 import emu.grasscutter.game.world.Scene;
 import emu.grasscutter.net.proto.VisionTypeOuterClass.VisionType;
 import emu.grasscutter.scripts.constants.EventType;
 import emu.grasscutter.scripts.data.SceneGroup;
 import emu.grasscutter.scripts.data.SceneMonster;
+import emu.grasscutter.scripts.data.ScriptArgs;
 import emu.grasscutter.server.packet.send.PacketChallengeDataNotify;
 import emu.grasscutter.server.packet.send.PacketDungeonChallengeBeginNotify;
 import emu.grasscutter.server.packet.send.PacketDungeonChallengeFinishNotify;
+import emu.grasscutter.server.packet.send.PacketDungeonSettleNotify;
+import emu.grasscutter.server.packet.send.PacketGadgetAutoPickDropInfoNotify;
 import emu.grasscutter.server.packet.send.PacketSceneEntityAppearNotify;
+import emu.grasscutter.utils.Utils;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
 
 public class DungeonChallenge {
 	private final Scene scene;
@@ -28,12 +39,12 @@ public class DungeonChallenge {
 	
 	private int score;
 	private int objective = 0;
+	private IntSet rewardedPlayers;
 	
 	public DungeonChallenge(Scene scene, SceneGroup group) {
 		this.scene = scene;
 		this.group = group;
-		
-		objective += group.monsters.size();
+		this.setRewardedPlayers(new IntOpenHashSet());
 	}
 
 	public Scene getScene() {
@@ -60,6 +71,14 @@ public class DungeonChallenge {
 		this.challengeId = challengeId;
 	}
 
+	public int getObjective() {
+		return objective;
+	}
+
+	public void setObjective(int objective) {
+		this.objective = objective;
+	}
+
 	public boolean isSuccess() {
 		return success;
 	}
@@ -75,6 +94,18 @@ public class DungeonChallenge {
 	public int getScore() {
 		return score;
 	}
+	
+	public int getTimeLimit() {
+		return 600;
+	}
+
+	public IntSet getRewardedPlayers() {
+		return rewardedPlayers;
+	}
+
+	public void setRewardedPlayers(IntSet rewardedPlayers) {
+		this.rewardedPlayers = rewardedPlayers;
+	}
 
 	public void start() {
 		this.progress = true;
@@ -83,13 +114,25 @@ public class DungeonChallenge {
 	
 	public void finish() {
 		this.progress = false;
+		
 		getScene().broadcastPacket(new PacketDungeonChallengeFinishNotify(this));
 		
 		if (this.isSuccess()) {
+			// Call success script event
 			this.getScene().getScriptManager().callEvent(EventType.EVENT_CHALLENGE_SUCCESS, null);
+			
+			// Settle
+			settle();
 		} else {
 			this.getScene().getScriptManager().callEvent(EventType.EVENT_CHALLENGE_FAIL, null);
 		}
+	}
+	
+	private void settle() {
+		getScene().setAutoCloseTime(Utils.getCurrentSeconds() + 1000);
+		getScene().broadcastPacket(new PacketDungeonSettleNotify(this));
+		
+		getScene().getScriptManager().callEvent(EventType.EVENT_DUNGEON_SETTLE, new ScriptArgs(this.isSuccess() ? 1 : 0));
 	}
 
 	public void onMonsterDie(EntityMonster entity) {
@@ -97,9 +140,31 @@ public class DungeonChallenge {
 		
 		getScene().broadcastPacket(new PacketChallengeDataNotify(this, 1, getScore()));
 		
-		if (getScore() >= objective) {
+		if (getScore() >= getObjective()) {
 			this.setSuccess(true);
 			finish();
 		}
+	}
+	
+	public void getStatueDrops(Player player) {
+		DungeonData dungeonData = getScene().getDungeonData();
+		if (!isSuccess() || dungeonData == null || dungeonData.getRewardPreview() == null || dungeonData.getRewardPreview().getPreviewItems().length == 0) {
+			return;
+		}
+		
+		// Already rewarded
+		if (getRewardedPlayers().contains(player.getUid())) {
+			return;
+		}
+		
+		List<GameItem> rewards = new ArrayList<>();
+		for (ItemParamData param : getScene().getDungeonData().getRewardPreview().getPreviewItems()) {
+			rewards.add(new GameItem(param.getId(), Math.max(param.getCount(), 1)));
+		}
+		
+		player.getInventory().addItems(rewards, ActionReason.DungeonStatueDrop);
+		player.sendPacket(new PacketGadgetAutoPickDropInfoNotify(rewards));
+		
+		getRewardedPlayers().add(player.getUid());
 	}
 }
