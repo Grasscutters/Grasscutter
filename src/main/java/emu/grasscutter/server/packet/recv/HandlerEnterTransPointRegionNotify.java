d62be2d1d7bd89712fa3cdfaad16be3b12b120e9
BaiSugar <97774724+BaiSugar@users.noreply.github.com>
2022-05-05 02:45:20 +0800

    Add the god statue's blood return display and stamina system (#520)
    * Fix announcement display
    
    * Approaching StatuesOfTheSeven will restore all health of the current team
    
    * Added god statue's blood return display and stamina system
    
    * fix error
    
    fix error
    
    * fix file

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerEnterTransPointRegionNotify.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerEnterTransPointRegionNotify.java
@@ -4,9 +4,15 @@ import emu.grasscutter.game.props.FightProperty;
 import emu.grasscutter.net.packet.Opcodes;
 import emu.grasscutter.net.packet.PacketHandler;
 import emu.grasscutter.net.packet.PacketOpcodes;
+import emu.grasscutter.net.proto.ChangeHpReasonOuterClass.ChangeHpReason;
+import emu.grasscutter.net.proto.PropChangeReasonOuterClass.PropChangeReason;
 import emu.grasscutter.server.game.GameSession;
 import emu.grasscutter.server.packet.send.PacketAvatarFightPropUpdateNotify;
 import emu.grasscutter.server.packet.send.PacketAvatarLifeStateChangeNotify;
+import emu.grasscutter.server.packet.send.PacketEntityFightPropChangeReasonNotify;
+import emu.grasscutter.server.packet.send.PacketEntityFightPropUpdateNotify;
+
+import java.util.List;
 
 @Opcodes(PacketOpcodes.EnterTransPointRegionNotify)
 public class HandlerEnterTransPointRegionNotify extends PacketHandler {
@@ -14,13 +20,23 @@ public class HandlerEnterTransPointRegionNotify extends PacketHandler {
     public void handle(GameSession session, byte[] header, byte[] payload) throws Exception{
         session.getPlayer().getTeamManager().getActiveTeam().forEach(entity -> {
             boolean isAlive = entity.isAlive();
-            entity.setFightProperty(
-                    FightProperty.FIGHT_PROP_CUR_HP,
-                    entity.getFightProperty(FightProperty.FIGHT_PROP_MAX_HP)
-            );
-            entity.getWorld().broadcastPacket(new PacketAvatarFightPropUpdateNotify(entity.getAvatar(), FightProperty.FIGHT_PROP_CUR_HP));
-            if (!isAlive) {
-                entity.getWorld().broadcastPacket(new PacketAvatarLifeStateChangeNotify(entity.getAvatar()));
+            if(entity.getFightProperty(FightProperty.FIGHT_PROP_CUR_HP) != entity.getFightProperty(FightProperty.FIGHT_PROP_MAX_HP)){
+                Float hp = entity.getFightProperty(FightProperty.FIGHT_PROP_MAX_HP)-entity.getFightProperty(FightProperty.FIGHT_PROP_CUR_HP);
+                
+                session.send(new PacketEntityFightPropUpdateNotify(entity,FightProperty.FIGHT_PROP_MAX_HP));
+
+                session.send(new PacketEntityFightPropChangeReasonNotify(
+                        entity, FightProperty.FIGHT_PROP_CUR_HP, hp, List.of(3),
+                        PropChangeReason.PROP_CHANGE_STATUE_RECOVER, ChangeHpReason.ChangeHpAddStatue));
+
+                entity.setFightProperty(
+                        FightProperty.FIGHT_PROP_CUR_HP,
+                        entity.getFightProperty(FightProperty.FIGHT_PROP_MAX_HP)
+                );
+                session.send(new PacketAvatarFightPropUpdateNotify(entity.getAvatar(), FightProperty.FIGHT_PROP_CUR_HP));
+                if (!isAlive) {
+                    entity.getWorld().broadcastPacket(new PacketAvatarLifeStateChangeNotify(entity.getAvatar()));
+                }
             }
         });
     }
