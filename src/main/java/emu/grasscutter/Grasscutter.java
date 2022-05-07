65cd76f65a96b6029600f7c6c78b13d80b44a023
33 <moe@3-3.dev>
2022-05-07 18:51:58 +0800

    Revert #611

--- src/main/java/emu/grasscutter/Grasscutter.java
+++ src/main/java/emu/grasscutter/Grasscutter.java
@@ -149,14 +149,8 @@ public final class Grasscutter {
 
 	public static void loadLanguage() {
 		var locale = config.LocaleLanguage;
-		String languageTag = locale.toLanguageTag();
-		if (languageTag.equals("und")) {
-			Grasscutter.getLogger().error("Illegal locale language, using en-US instead.");
-			language = Language.getLanguage("en-US");
-		} else {
-			languageTag = String.format("%s-%s", locale.getLanguage(), locale.getCountry());
-			language = Language.getLanguage(languageTag);
-		}
+		String languageTag = String.format("%s-%s", locale.getLanguage(), locale.getCountry());
+		language = Language.getLanguage(languageTag);
 	}
 
 	public static void saveConfig() {
