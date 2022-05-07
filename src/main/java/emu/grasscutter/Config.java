f9b5478c9de2fb35acce628fead788c81ae6accf
Secretboy <74841174+Secretboy-SMR@users.noreply.github.com>
2022-05-07 12:48:53 +0800

    Update Config.java

--- src/main/java/emu/grasscutter/Config.java
+++ src/main/java/emu/grasscutter/Config.java
@@ -23,6 +23,7 @@ public final class Config {
 	public GameServerOptions GameServer = new GameServerOptions();
 	public DispatchServerOptions DispatchServer = new DispatchServerOptions();
 	public Locale LocaleLanguage = Locale.getDefault();
+	public Locale DefaultLanguage = Locale.US;
 
 	public Boolean OpenStamina = true;
 	public GameServerOptions getGameServerOptions() {
