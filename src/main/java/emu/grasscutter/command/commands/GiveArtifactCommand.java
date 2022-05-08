2a12ed5694ca474782b4fde96b9e637c24bf23b4
ImmuState <kyoko12@gmx.at>
2022-05-08 09:18:53 -0700

    Fix incorrect @Command annotation.

--- src/main/java/emu/grasscutter/command/commands/GiveArtifactCommand.java
+++ src/main/java/emu/grasscutter/command/commands/GiveArtifactCommand.java
@@ -19,7 +19,7 @@ import static java.util.Map.entry;
 
 import static emu.grasscutter.utils.Language.translate;
 
-@Command(label = "giveart", usage = "giveart <artifactId> <mainPropId> [<appendPropId>[,<times>]]... [level]", description = "Gives the player a specified artifact", aliases = {"gart"}, permission = "player.giveart")
+@Command(label = "giveart", usage = "giveart <artifactId> <mainPropId> [<appendPropId>[,<times>]]... [level]", aliases = {"gart"}, permission = "player.giveart", description = "commands.giveArtifact.description")
 public final class GiveArtifactCommand implements CommandHandler {
 	private static final Map<String, Map<EquipType, Integer>> mainPropMap = Map.ofEntries(
 		entry("hp", Map.ofEntries(entry(EquipType.EQUIP_BRACER, 14001))),
