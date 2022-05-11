98066f5015f978127177ad328045425bb762b85d
Secretboy-SMR <secretboy.smr@icloud.com>
2022-05-11 18:06:37 +0800

    Removed invalid code in getLanguageFileDescription,When the language is not discovered, it will use the built-in language fallback mechanism to fall back,At the same time, we also fix the issue that using language in the server does not save the settings of the server side locale

--- src/main/java/emu/grasscutter/command/commands/LanguageCommand.java
+++ src/main/java/emu/grasscutter/command/commands/LanguageCommand.java
@@ -31,24 +31,24 @@ public final class LanguageCommand implements CommandHandler {
         }
 
         String langCode = args.get(0);
-        String actualLangCode = null;
+
+        var languageInst = Grasscutter.getLanguage(langCode);
+        var actualLangCode = languageInst.getLanguageCode();
+        var locale = Locale.forLanguageTag(actualLangCode);
         if (sender != null) {
-            var locale = Locale.forLanguageTag(langCode);
-            actualLangCode = Utils.getLanguageCode(locale);
             var account = sender.getAccount();
             account.setLocale(locale);
             account.save();
         }
         else {
-            var languageInst = Grasscutter.getLanguage(langCode);
-            actualLangCode = languageInst.getLanguageCode();
             Grasscutter.setLanguage(languageInst);
+            var config = Grasscutter.getConfig();
+            config.language.language = locale;
+            Grasscutter.saveConfig(config);
         }
 
         if (!langCode.equals(actualLangCode)) {
-            // I think there is no necessary to register this in language files
-            // since this will always be english
-            CommandHandler.sendMessage(sender, "currently, server does not have that language: " + langCode);
+            CommandHandler.sendMessage(sender, translate(sender, "commands.language.language_not_found", langCode));
         }
 
         CommandHandler.sendMessage(sender, translate(sender, "commands.language.language_changed", actualLangCode));
