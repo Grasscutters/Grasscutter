bc0da85e79caa1ef74ce283803d4e420733e2ec9
mingjun97 <my@lyric.today>
2022-05-10 23:23:58 -0700

    Revise version format

--- src/main/java/emu/grasscutter/Grasscutter.java
+++ src/main/java/emu/grasscutter/Grasscutter.java
@@ -81,7 +81,7 @@ public final class Grasscutter {
 					Tools.createGachaMapping(Grasscutter.getConfig().DATA_FOLDER + "/gacha_mappings.js"); exitEarly = true;
 				}
 				case "-version" -> {
-					System.out.println("Grasscutter version: " + BuildConfig.VERSION + "\nGit Hash: " + BuildConfig.GIT_HASH); exitEarly = true;
+					System.out.println("Grasscutter version: " + BuildConfig.VERSION + "-" + BuildConfig.GIT_HASH); exitEarly = true;
 				}
 			}
 		} 
