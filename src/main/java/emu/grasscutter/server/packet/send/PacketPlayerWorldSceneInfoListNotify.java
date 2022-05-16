58a5ad4909c7b5bc66f19856a49962bd99324bf4
Elapse <75983455+elap5e@users.noreply.github.com>
2022-05-16 19:10:58 +0800

    update opcodes, correct op names, and update relative codes

--- src/main/java/emu/grasscutter/server/packet/send/PacketPlayerWorldSceneInfoListNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketPlayerWorldSceneInfoListNotify.java
@@ -2,15 +2,15 @@ package emu.grasscutter.server.packet.send;
 
 import emu.grasscutter.net.packet.BasePacket;
 import emu.grasscutter.net.packet.PacketOpcodes;
-import emu.grasscutter.net.proto.SceneUnlockInfoNotifyOuterClass.SceneUnlockInfoNotify;
+import emu.grasscutter.net.proto.PlayerWorldSceneInfoListNotifyOuterClass.PlayerWorldSceneInfoListNotify;
 import emu.grasscutter.net.proto.SceneUnlockInfoOuterClass.SceneUnlockInfo;
 
-public class PacketSceneUnlockInfoNotify extends BasePacket {
+public class PacketPlayerWorldSceneInfoListNotify extends BasePacket {
 	
-	public PacketSceneUnlockInfoNotify() {
-		super(PacketOpcodes.SceneUnlockInfoNotify); // Rename opcode later
+	public PacketPlayerWorldSceneInfoListNotify() {
+		super(PacketOpcodes.PlayerWorldSceneInfoListNotify); // Rename opcode later
 
-		SceneUnlockInfoNotify proto = SceneUnlockInfoNotify.newBuilder()
+		PlayerWorldSceneInfoListNotify proto = PlayerWorldSceneInfoListNotify.newBuilder()
 				.addUnlockInfos(SceneUnlockInfo.newBuilder().setSceneId(1))
 				.addUnlockInfos(SceneUnlockInfo.newBuilder().setSceneId(3).addSceneTagIdList(102).addSceneTagIdList(113).addSceneTagIdList(117))
 				.addUnlockInfos(SceneUnlockInfo.newBuilder().setSceneId(4).addSceneTagIdList(106).addSceneTagIdList(109))
