231cc3fef65eedb4831d8ba952c3429f2d657704
lunaticwhat <sickyturtle@gmail.com>
2022-05-17 20:35:26 +0700

    Issue template updated no more headache

---
+++ src/main/java/emu/grasscutter/BuildConfig.java
@@ -0,0 +1,7 @@
+
+        package emu.grasscutter;
+        public class BuildConfig {
+                public static final String VERSION = "1.1.2-dev";
+                public static final String GIT_HASH = "b0aa294";
+        }
+    
