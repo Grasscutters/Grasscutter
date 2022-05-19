88d57086b35b586ebbedd058948293c5f609c4a0
litht <2021294415@qq.com>
2022-05-19 13:28:00 +0800

    修复了打jar包后，运行找不到主类的错误

--- src/main/java/emu/grasscutter/server/http/dispatch/cn/HttpProxy.java
+++ src/main/java/emu/grasscutter/server/http/dispatch/cn/HttpProxy.java
@@ -2,7 +2,11 @@ package emu.grasscutter.server.http.dispatch.cn;
 
 import com.github.monkeywie.proxyee.server.HttpProxyServer;
 import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
-import static emu.grasscutter.Configuration.*;
+import emu.grasscutter.Grasscutter;
+
+import static emu.grasscutter.Configuration.PROXY_INFO;
+import static emu.grasscutter.Configuration.lr;
+import static emu.grasscutter.utils.Language.translate;
 
 /**
  * @author litht
@@ -33,9 +37,13 @@ public class HttpProxy {
      * 启动
      */
     public void start() {
-        Thread thread=new Thread(()->{
+        Thread thread = new Thread(() -> {
             server.start(PROXY_INFO.bindPort);
         });
+        String url = "https://" + PROXY_INFO.accessAddress + ":" + lr(PROXY_INFO.accessPort, PROXY_INFO.bindPort);
+        Grasscutter.getLogger().info(translate("messages.proxy.port", PROXY_INFO.accessAddress, lr(PROXY_INFO.accessPort, PROXY_INFO.bindPort)));
+        Grasscutter.getLogger().info(translate("messages.proxy.cert", url));
+        Grasscutter.getLogger().info(translate("messages.proxy.proxy", PROXY_INFO.accessAddress, lr(PROXY_INFO.accessPort, PROXY_INFO.bindPort)));
         thread.start();
 
     }
