b60e9eb3ff4cc1c01acfae6cc0ca8b2c5f752729
ShiroSaki <62388797+ShigemoriHakura@users.noreply.github.com>
2022-05-22 17:28:52 +0800

    Fix PacketQueryCodexMonsterBeKilledNumRsp

--- src/main/java/emu/grasscutter/server/packet/send/PacketQueryCodexMonsterBeKilledNumRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketQueryCodexMonsterBeKilledNumRsp.java
@@ -5,8 +5,6 @@ import emu.grasscutter.net.packet.BasePacket;
 import emu.grasscutter.net.packet.PacketOpcodes;
 import emu.grasscutter.net.proto.QueryCodexMonsterBeKilledNumRspOuterClass.QueryCodexMonsterBeKilledNumRsp;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class PacketQueryCodexMonsterBeKilledNumRsp extends BasePacket {
@@ -15,10 +13,11 @@ public class PacketQueryCodexMonsterBeKilledNumRsp extends BasePacket {
         super(PacketOpcodes.QueryCodexMonsterBeKilledNumRsp);
         QueryCodexMonsterBeKilledNumRsp.Builder proto = QueryCodexMonsterBeKilledNumRsp.newBuilder();
 
-        //I don't know why it's not working!
         codexList.forEach(animal -> {
             if(player.getCodex().getUnlockedAnimal().containsKey(animal)){
-                proto.addCodexIdList(animal).addBeKilledNumList(player.getCodex().getUnlockedAnimal().get(animal));
+                proto.addCodexIdList(animal)
+                        .addBeKilledNumList(player.getCodex().getUnlockedAnimal().get(animal))
+                        .addBeKilledNumEmptyList(0);
             }
         });
 
