e59fe19685bd88e09aa0663597be67eb0d51ad87
NinjaTester0021 <105205048+NinjaTester0021@users.noreply.github.com>
2022-06-01 14:02:41 +0530

    Update java.java
    unsed variable

--- java.java
+++ java.java
@@ -1,6 +1,6 @@
 public class PrimeExample{  
  public static void main(String args[]){  
-  int i,m=0,flag=0;    
+  int i,m=0,flag=0,int c;    
   int n=3;//it is the number to be checked  
   m=n/2;    
   if(n==0||n==1){
