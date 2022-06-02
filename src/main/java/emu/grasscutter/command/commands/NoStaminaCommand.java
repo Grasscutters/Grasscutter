71b53e0be4f5112c2e06d6defc94ee535c5adad3
tamilpp25 <tamilpp25@gmail.com>
2022-06-02 16:03:46 +0530

    Adding toggle feature to "nostamina" command similar to "godmode" command (#1152)
    * stamina command enhancement
    
    * added "ns" as command aliases for NoStaminaCommand

--- src/main/java/emu/grasscutter/command/commands/NoStaminaCommand.java
+++ src/main/java/emu/grasscutter/command/commands/NoStaminaCommand.java
@@ -9,26 +9,26 @@ import java.util.List;
 import static emu.grasscutter.utils.Language.translate;
 
 
-@Command(label = "nostamina", usage = "nostamina [on|off]", permission = "player.nostamina", permissionTargeted = "player.nostamina.others", description = "commands.nostamina.description")
+@Command(label = "nostamina", usage = "nostamina [on|off|toggle]",aliases = {"ns"},permission = "player.nostamina", permissionTargeted = "player.nostamina.others", description = "commands.nostamina.description")
 public final class NoStaminaCommand implements CommandHandler {
-    public static boolean StaminaState = false;
-    //Temp Value
     @Override
     public void execute(Player sender, Player targetPlayer, List<String> args) {
+        boolean stamina = !targetPlayer.getStamina();
         if (args.size() == 1) {
             switch (args.get(0).toLowerCase()) {
                 case "on":
-                    StaminaState = true;
+                    stamina = true;
                     break;
                 case "off":
-                    StaminaState = false;
+                    stamina = false;
                     break;
                 default:
+                    //toggled
                     break;
             }
         }
-        targetPlayer.setStamina(StaminaState);//Set
+        targetPlayer.setStamina(stamina);//Set
 
-        CommandHandler.sendMessage(sender, translate(sender, "commands.nostamina.success", (StaminaState ? translate(sender, "commands.status.enabled") : translate(sender, "commands.status.disabled")), targetPlayer.getNickname()));
+        CommandHandler.sendMessage(sender, translate(sender, "commands.nostamina.success", (stamina ? translate(sender, "commands.status.enabled") : translate(sender, "commands.status.disabled")), targetPlayer.getNickname()));
     }
 }
