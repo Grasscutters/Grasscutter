8bc5a167e0efdcfec988e2cbdfc67d1567839113
AZthemute <104761702+AZthemute@users.noreply.github.com>
2022-06-03 14:27:53 +0100

    When the server starts, it now outputs the game and server version. Too dumb to not hardcode it - sorry!

--- src/main/java/emu/grasscutter/server/game/GameServer.java
+++ src/main/java/emu/grasscutter/server/game/GameServer.java
@@ -70,6 +70,7 @@ public final class GameServer extends KcpServer {
 		}
 		return inetSocketAddress;
 	}
+
 	public GameServer() {
 		this(getAdapterInetSocketAddress());
 	}
@@ -268,7 +269,10 @@ public final class GameServer extends KcpServer {
 
 	@Override
 	public void onStartFinish() {
+		String version = GameConstants.VERSION;
 		Grasscutter.getLogger().info(translate("messages.status.free_software"));
+		Grasscutter.getLogger().info(translate("messages.status.game_version"));
+		Grasscutter.getLogger().info(translate("messages.status.version"));
 		Grasscutter.getLogger().info(translate("messages.game.port_bind", Integer.toString(address.getPort())));
 		ServerStartEvent event = new ServerStartEvent(ServerEvent.Type.GAME, OffsetDateTime.now()); event.call();
 	}
