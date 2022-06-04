4ffda39b1265ff40f7096cd82607a96b08a3d587
ImmuState <kyoko12@gmx.at>
2022-06-04 12:05:03 -0700

    Add handling for ForgeExcelConfigData.

---
+++ src/main/java/emu/grasscutter/data/common/MaterialItemsData.java
@@ -0,0 +1,22 @@
+package emu.grasscutter.data.common;
+
+import java.util.List;
+
+public class MaterialItemsData {
+	private int id;
+	private int count;
+
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int value) {
+		this.id = value;
+	}
+
+	public int getCount() {
+		return this.count;
+	}
+	public void setCount(int value) {
+		this.count = value;
+	}
+}
