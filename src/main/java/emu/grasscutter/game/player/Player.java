afa16f90f43d052f20e72e0534e9359a5bbb8997
ImmuState <kyoko12@gmx.at>
2022-06-06 16:55:29 -0700

    Send periodic notifications to the client for ongoing forges.

--- src/main/java/emu/grasscutter/game/player/Player.java
+++ src/main/java/emu/grasscutter/game/player/Player.java
@@ -1193,6 +1193,9 @@ public class Player {
 			this.save();
 			this.sendPacket(new PacketAvatarExpeditionDataNotify(this));
 		}
+
+		// Send updated forge queue data, if necessary.
+		this.getForgingManager().sendPlayerForgingUpdate();
 	}
 
 
