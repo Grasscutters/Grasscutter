1136a5dd4f89a29e7465dd8fe92c121a7afe1cb9
Melledy <52122272+Melledy@users.noreply.github.com>
2022-06-07 03:27:43 -0700

    Delete BuildConfig.java

--- src/main/java/emu/grasscutter/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-
-        package emu.grasscutter;
-        public class BuildConfig {
-                public static final String VERSION = "1.1.2-dev";
-                public static final String GIT_HASH = "b0aa294";
-        }
-    
