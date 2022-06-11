905976e007c237b0ef358ca8e1cc5a75241c296b
KingRainbow44 <kobedo11@gmail.com>
2022-06-11 15:31:29 -0400

    Remove un-needed lines in `GameServer`

--- src/main/java/emu/grasscutter/server/game/GameServer.java
+++ src/main/java/emu/grasscutter/server/game/GameServer.java
@@ -27,7 +27,8 @@ import emu.grasscutter.server.event.game.ServerTickEvent;
 import emu.grasscutter.server.event.internal.ServerStartEvent;
 import emu.grasscutter.server.event.internal.ServerStopEvent;
 import emu.grasscutter.task.TaskMap;
-import emu.grasscutter.BuildConfig;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.net.InetSocketAddress;
 import java.time.OffsetDateTime;
@@ -39,41 +40,38 @@ import static emu.grasscutter.Configuration.*;
 
 public final class GameServer extends KcpServer {
 	private final InetSocketAddress address;
-	private final GameServerPacketHandler packetHandler;
-	private final ServerQuestHandler questHandler;
+	@Getter private final GameServerPacketHandler packetHandler;
+	@Getter private final ServerQuestHandler questHandler;
 	
 	private final Map<Integer, Player> players;
 	private final Set<World> worlds;
-	
-	private ChatManagerHandler chatManager;
-	private final InventoryManager inventoryManager;
-	private final GachaManager gachaManager;
-	private final ShopManager shopManager;
-	private final MultiplayerManager multiplayerManager;
-	private final DungeonManager dungeonManager;
-	private final ExpeditionManager expeditionManager;
-	private final CommandMap commandMap;
-	private final TaskMap taskMap;
-	private final DropManager dropManager;
 
-	private final CombineManger combineManger;
-	private final TowerScheduleManager towerScheduleManager;
+	@Getter @Setter private ChatManagerHandler chatManager;
+	@Getter private final InventoryManager inventoryManager;
+	@Getter private final GachaManager gachaManager;
+	@Getter private final ShopManager shopManager;
+	@Getter private final MultiplayerManager multiplayerManager;
+	@Getter private final DungeonManager dungeonManager;
+	@Getter private final ExpeditionManager expeditionManager;
+	@Getter private final CommandMap commandMap;
+	@Getter private final TaskMap taskMap;
+	@Getter private final DropManager dropManager;
 
-	private static InetSocketAddress getAdapterInetSocketAddress(){
-		InetSocketAddress inetSocketAddress = null;
-		if(GAME_INFO.bindAddress.equals("")){
-			inetSocketAddress=new InetSocketAddress(GAME_INFO.bindPort);
-		}else{
-			inetSocketAddress=new InetSocketAddress(
+	@Getter private final CombineManger combineManger;
+	@Getter private final TowerScheduleManager towerScheduleManager;
+
+	private static InetSocketAddress getAdapterInetSocketAddress() {
+		return GAME_INFO.bindAddress.equals("") ? 
+				new InetSocketAddress(GAME_INFO.bindPort) : 
+				new InetSocketAddress(
 					GAME_INFO.bindAddress,
-					GAME_INFO.bindPort
-			);
-		}
-		return inetSocketAddress;
+					GAME_INFO.bindPort);
 	}
+	
 	public GameServer() {
 		this(getAdapterInetSocketAddress());
 	}
+	
 	public GameServer(InetSocketAddress address) {
 		super(address);
 
@@ -84,6 +82,7 @@ public final class GameServer extends KcpServer {
 		this.players = new ConcurrentHashMap<>();
 		this.worlds = Collections.synchronizedSet(new HashSet<>());
 		
+		// Initialize managers.
 		this.chatManager = new ChatManager(this);
 		this.inventoryManager = new InventoryManager(this);
 		this.gachaManager = new GachaManager(this);
@@ -96,16 +95,9 @@ public final class GameServer extends KcpServer {
 		this.expeditionManager = new ExpeditionManager(this);
 		this.combineManger = new CombineManger(this);
 		this.towerScheduleManager = new TowerScheduleManager(this);
+		
 		// Hook into shutdown event.
 		Runtime.getRuntime().addShutdownHook(new Thread(this::onServerShutdown));
-	}
-	
-	public GameServerPacketHandler getPacketHandler() {
-		return packetHandler;
-	}
-
-	public ServerQuestHandler getQuestHandler() {
-		return questHandler;
 	}
 
 	public Map<Integer, Player> getPlayers() {
@@ -114,58 +106,6 @@ public final class GameServer extends KcpServer {
 
 	public Set<World> getWorlds() {
 		return worlds;
-	}
-
-	public ChatManagerHandler getChatManager() {
-		return chatManager;
-	}
-	
-	public void setChatManager(ChatManagerHandler chatManager) {
-		this.chatManager = chatManager;
-	}
-
-	public InventoryManager getInventoryManager() {
-		return inventoryManager;
-	}
-
-	public GachaManager getGachaManager() {
-		return gachaManager;
-	}
-	
-	public ShopManager getShopManager() {
-		return shopManager;
-	}
-
-	public MultiplayerManager getMultiplayerManager() {
-		return multiplayerManager;
-	}
-
-	public DropManager getDropManager() {
-		return dropManager;
-	}
-	
-	public DungeonManager getDungeonManager() {
-		return dungeonManager;
-	}
-
-	public ExpeditionManager getExpeditionManager() {
-		return expeditionManager;
-	}
-
-	public CommandMap getCommandMap() {
-		return this.commandMap;
-	}
-
-	public CombineManger getCombineManger(){
-		return this.combineManger;
-	}
-
-	public TowerScheduleManager getTowerScheduleManager() {
-		return towerScheduleManager;
-	}
-
-	public TaskMap getTaskMap() {
-		return this.taskMap;
 	}
 	
 	public void registerPlayer(Player player) {
@@ -221,7 +161,7 @@ public final class GameServer extends KcpServer {
 		return DatabaseHelper.getAccountByName(username);
 	}
 	
-	public void onTick() throws Exception {
+	public void onTick() {
 		Iterator<World> it = this.getWorlds().iterator();
 		while (it.hasNext()) {
 			World world = it.next();
