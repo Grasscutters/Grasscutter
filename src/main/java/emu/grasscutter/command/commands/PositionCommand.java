c5b45358e74a275d5ec8bea6026b8ea00015a65b
Tesutarin <105267106+Tesutarin@users.noreply.github.com>
2022-06-12 10:44:13 +0800

    Command: Add permissionTargeted or targetRequirement

--- src/main/java/emu/grasscutter/command/commands/PositionCommand.java
+++ src/main/java/emu/grasscutter/command/commands/PositionCommand.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static emu.grasscutter.utils.Language.translate;
 
-@Command(label = "position", usage = "position", aliases = {"pos"}, description = "commands.position.description")
+@Command(label = "position", usage = "position", aliases = {"pos"}, description = "commands.position.description", targetRequirement = Command.TargetRequirement.NONE)
 public final class PositionCommand implements CommandHandler {
 
     @Override
