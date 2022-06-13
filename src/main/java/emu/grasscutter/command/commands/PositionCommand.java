2560b14557e104ab9d1e73e1f04216fe1676bc14
Luke Hubmayer-Werner <ld.hubmayer.werner@gmail.com>
2022-06-13 12:02:36 +0930

    Partial revert of #1245

--- src/main/java/emu/grasscutter/command/commands/PositionCommand.java
+++ src/main/java/emu/grasscutter/command/commands/PositionCommand.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static emu.grasscutter.utils.Language.translate;
 
-@Command(label = "position", usage = "position", aliases = {"pos"}, description = "commands.position.description", targetRequirement = Command.TargetRequirement.NONE)
+@Command(label = "position", usage = "position", aliases = {"pos"}, description = "commands.position.description")
 public final class PositionCommand implements CommandHandler {
 
     @Override
