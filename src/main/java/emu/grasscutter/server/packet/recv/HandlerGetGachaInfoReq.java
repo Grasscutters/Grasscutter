8f4f1887d90d8ef8a42bfd1494e8774111d0b777
CamChua_VN <32415041+CamChuaVN@users.noreply.github.com>
2022-06-16 22:01:27 +0700

    Update Epitomized Path (#1254)
    * Update Epitomized Path
    
    * Update Epitomized Path
    
    * Update Epitomized Path
    
    * Refactor doRarePull
    
    * Update Epitomized Path
    
    Co-authored-by: AnimeGitUserB <AnimeGitUserB@bigblueball.in>

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerGetGachaInfoReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerGetGachaInfoReq.java
@@ -1,9 +1,12 @@
 package emu.grasscutter.server.packet.recv;
 
+import emu.grasscutter.game.gacha.GachaBanner;
+import emu.grasscutter.game.gacha.PlayerGachaBannerInfo;
 import emu.grasscutter.net.packet.Opcodes;
 import emu.grasscutter.net.packet.PacketOpcodes;
 import emu.grasscutter.net.packet.PacketHandler;
 import emu.grasscutter.server.game.GameSession;
+import emu.grasscutter.server.packet.send.PacketGachaWishRsp;
 import emu.grasscutter.server.packet.send.PacketGetGachaInfoRsp;
 
 @Opcodes(PacketOpcodes.GetGachaInfoReq)
