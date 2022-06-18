9daf89b953228b8b0e548be6cfd207ab63c4e5f9
Melledy <52122272+Melledy@users.noreply.github.com>
2022-06-17 18:58:32 -0700

    Move ban check to GetPlayerTokenReq

--- src/main/java/emu/grasscutter/server/packet/send/PacketGetPlayerTokenRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGetPlayerTokenRsp.java
@@ -15,37 +15,40 @@ public class PacketGetPlayerTokenRsp extends BasePacket {
 		
 		this.setUseDispatchKey(true);
 
-		if (session.getPlayer().getAccount().isBanned()) {
-
-			GetPlayerTokenRsp p = GetPlayerTokenRsp.newBuilder()
+		GetPlayerTokenRsp p = GetPlayerTokenRsp.newBuilder()
+			.setUid(session.getPlayer().getUid())
+			.setToken(session.getAccount().getToken())
+			.setAccountType(1)
+			.setIsProficientPlayer(session.getPlayer().getAvatars().getAvatarCount() > 0) // Not sure where this goes
+			.setSecretKeySeed(Crypto.ENCRYPT_SEED)
+			.setSecurityCmdBuffer(ByteString.copyFrom(Crypto.ENCRYPT_SEED_BUFFER))
+			.setPlatformType(3)
+			.setChannelId(1)
+			.setCountryCode("US")
+			.setClientVersionRandomKey("c25-314dd05b0b5f")
+			.setRegPlatform(3)
+			.setClientIpStr(session.getAddress().getAddress().getHostAddress())
+			.build();
+		
+		this.setData(p.toByteArray());
+	}
+	
+	public PacketGetPlayerTokenRsp(GameSession session, int retcode, String msg, int blackEndTime) {
+		super(PacketOpcodes.GetPlayerTokenRsp, true);
+		
+		this.setUseDispatchKey(true);
+		
+		GetPlayerTokenRsp p = GetPlayerTokenRsp.newBuilder()
 				.setUid(session.getPlayer().getUid())
 				.setIsProficientPlayer(session.getPlayer().getAvatars().getAvatarCount() > 0)
-				.setRetcode(21)
-				.setMsg("FORBID_CHEATING_PLUGINS")
-				.setBlackUidEndTime(session.getPlayer().getAccount().getBanEndTime())
+				.setRetcode(retcode)
+				.setMsg(msg)
+				.setBlackUidEndTime(blackEndTime)
 				.setRegPlatform(3)
 				.setCountryCode("US")
 				.setClientIpStr(session.getAddress().getAddress().getHostAddress())
 				.build();
 			
-			this.setData(p.toByteArray());
-		} else {
-			GetPlayerTokenRsp p = GetPlayerTokenRsp.newBuilder()
-				.setUid(session.getPlayer().getUid())
-				.setToken(session.getAccount().getToken())
-				.setAccountType(1)
-				.setIsProficientPlayer(session.getPlayer().getAvatars().getAvatarCount() > 0) // Not sure where this goes
-				.setSecretKeySeed(Crypto.ENCRYPT_SEED)
-				.setSecurityCmdBuffer(ByteString.copyFrom(Crypto.ENCRYPT_SEED_BUFFER))
-				.setPlatformType(3)
-				.setChannelId(1)
-				.setCountryCode("US")
-				.setClientVersionRandomKey("c25-314dd05b0b5f")
-				.setRegPlatform(3)
-				.setClientIpStr(session.getAddress().getAddress().getHostAddress())
-				.build();
-			
-			this.setData(p.toByteArray());
-		}
+		this.setData(p.toByteArray());
 	}
 }
