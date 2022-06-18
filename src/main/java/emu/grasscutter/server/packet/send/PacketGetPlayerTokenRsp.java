a2de0e237663e26f67cb10eaa8d8ea2ecbc45e7f
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-06-18 10:10:42 +0900

    no more bin

--- src/main/java/emu/grasscutter/server/packet/send/PacketGetPlayerTokenRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGetPlayerTokenRsp.java
@@ -1,7 +1,5 @@
 package emu.grasscutter.server.packet.send;
 
-import java.util.Base64;
-
 import com.google.protobuf.ByteString;
 
 import emu.grasscutter.net.packet.BasePacket;
@@ -18,13 +16,11 @@ public class PacketGetPlayerTokenRsp extends BasePacket {
 		this.setUseDispatchKey(true);
 
 		if (session.getPlayer().getAccount().isBanned()) {
-			byte[] bin = Base64.getDecoder().decode("5LiN54ixIEdDIOWNgeW5tOS6hu+8jOWNgeW5tOmHjOeUqOi/h+eahOavj+S4gOS4qiBQUyDpg73lg48gR0PjgII=");
 
 			GetPlayerTokenRsp p = GetPlayerTokenRsp.newBuilder()
 				.setUid(session.getPlayer().getUid())
 				.setIsProficientPlayer(session.getPlayer().getAvatars().getAvatarCount() > 0)
 				.setRetcode(21)
-				.setExtraBinData(ByteString.copyFrom(bin))
 				.setMsg("FORBID_CHEATING_PLUGINS")
 				.setBlackUidEndTime(session.getPlayer().getAccount().getBanEndTime())
 				.setRegPlatform(3)
