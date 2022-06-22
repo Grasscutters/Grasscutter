371acb9ba51d8408449b8d415ecf559aec8bd3e7
AnimeGitB <AnimeGitB@bigblueball.in>
2022-06-22 23:20:16 +0930

    Deduplicate some translation keys

--- src/main/java/emu/grasscutter/command/CommandMap.java
+++ src/main/java/emu/grasscutter/command/CommandMap.java
@@ -156,7 +156,7 @@ public final class CommandMap {
                         CommandHandler.sendTranslatedMessage(player, targetPlayer.isOnline()? "commands.execution.set_target_online" : "commands.execution.set_target_offline", targetUidStr);
                     }
                 } catch (NumberFormatException e) {
-                    CommandHandler.sendTranslatedMessage(player, "commands.execution.uid_error");
+                    CommandHandler.sendTranslatedMessage(player, "commands.generic.invalid.uid");
                 }
             }
             return;
@@ -183,7 +183,7 @@ public final class CommandMap {
                     }
                     break;
                 } catch (NumberFormatException e) {
-                    CommandHandler.sendTranslatedMessage(player, "commands.execution.uid_error");
+                    CommandHandler.sendTranslatedMessage(player, "commands.generic.invalid.uid");
                     return;
                 }
             }
