34d6959e5220d917c971aef24c544863b038be91
zhaodice <63996691+zhaodice@users.noreply.github.com>
2022-06-26 15:08:06 +0800

    Avoid disconnection notification

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerPlayerForceExitReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerPlayerForceExitReq.java
@@ -12,6 +12,17 @@ public class HandlerPlayerForceExitReq extends PacketHandler {
 	public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
 		// Client should auto disconnect right now
 		session.send(new BasePacket(PacketOpcodes.PlayerForceExitRsp));
-		session.close();
+		new Thread(){
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);// disconnect after 1 seconds
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				session.close();
+				super.run();
+			}
+		}.start();
 	}
 }
