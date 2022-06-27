a8690ef7200082114efce4bd729d0380885abaab
Akka <104902222+Akka0@users.noreply.github.com>
2022-06-27 22:40:12 +0800

    fix bug

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerMusicGameCreateBeatmapReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerMusicGameCreateBeatmapReq.java
@@ -1,7 +1,9 @@
 package emu.grasscutter.server.packet.recv;
 
+import emu.grasscutter.database.DatabaseHelper;
 import emu.grasscutter.game.activity.musicgame.MusicGameActivityHandler;
 import emu.grasscutter.game.activity.musicgame.MusicGameBeatmap;
+import emu.grasscutter.game.activity.musicgame.MusicGamePlayerData;
 import emu.grasscutter.game.props.ActivityType;
 import emu.grasscutter.net.packet.Opcodes;
 import emu.grasscutter.net.packet.PacketHandler;
@@ -11,6 +13,8 @@ import emu.grasscutter.server.game.GameSession;
 import emu.grasscutter.server.packet.send.PacketActivityInfoNotify;
 import emu.grasscutter.server.packet.send.PacketMusicGameCreateBeatmapRsp;
 import emu.grasscutter.utils.Utils;
+
+import java.util.Objects;
 
 @Opcodes(PacketOpcodes.MusicGameCreateBeatmapReq)
 public class HandlerMusicGameCreateBeatmapReq extends PacketHandler {
@@ -22,14 +26,13 @@ public class HandlerMusicGameCreateBeatmapReq extends PacketHandler {
         var musicGameBeatmap = MusicGameBeatmap.of()
             .musicId(req.getMusicBriefInfo().getMusicId())
             .musicNoteCount(req.getMusicBriefInfo().getMusicNoteCount())
-            .savePosition(req.getMusicBriefInfo().getSavePosition())
+            .savePosition(req.getMusicBriefInfo().getPosition())
             .maxScore(req.getMusicBriefInfo().getMaxScore())
             .authorUid(session.getPlayer().getUid())
             .beatmap(MusicGameBeatmap.parse(req.getMusicRecord().getBeatmapItemListList()))
             .createTime(Utils.getCurrentSeconds())
             .build();
 
-        // TODO avoid player save too much to make server down
         musicGameBeatmap.save();
 
         var playerData = session.getPlayer().getActivityManager().getPlayerActivityDataByActivityType(ActivityType.NEW_ACTIVITY_MUSIC_GAME);
@@ -38,6 +41,22 @@ public class HandlerMusicGameCreateBeatmapReq extends PacketHandler {
         }
 
         var handler = (MusicGameActivityHandler) playerData.get().getActivityHandler();
+        var musicGamePlayerData = handler.getMusicGamePlayerData(playerData.get());
+
+        var oldBeatmap = musicGamePlayerData.getPersonalCustomBeatmapRecord().values().stream()
+            .map(MusicGamePlayerData.CustomBeatmapRecord::getMusicShareId)
+            .map(DatabaseHelper::getMusicGameBeatmap)
+            .filter(Objects::nonNull)
+            .filter(item -> item.getAuthorUid() == session.getPlayer().getUid())
+            .filter(item -> item.getMusicId() == req.getMusicBriefInfo().getMusicId())
+            .filter(item -> item.getSavePosition() == req.getMusicBriefInfo().getPosition())
+            .findFirst();
+
+        // delete old beatmap for player
+        // the old beatmap is still in database so that others can still play.
+        oldBeatmap.ifPresent(i -> handler.removePersonalBeatmap(playerData.get(), i));
+
+        // link this beatmap to player's personal data
         handler.addPersonalBeatmap(playerData.get(), musicGameBeatmap);
 
         session.send(new PacketActivityInfoNotify(handler.toProto(playerData.get())));
---
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerMusicGameStartToPlayOthersBeatmapReq.java
@@ -0,0 +1,20 @@
+package emu.grasscutter.server.packet.recv;
+
+import emu.grasscutter.net.packet.Opcodes;
+import emu.grasscutter.net.packet.PacketHandler;
+import emu.grasscutter.net.packet.PacketOpcodes;
+import emu.grasscutter.net.proto.MusicGameStartToPlayOthersBeatmapReqOuterClass;
+import emu.grasscutter.server.game.GameSession;
+import emu.grasscutter.server.packet.send.PacketMusicGameStartToPlayOthersBeatmapRsp;
+
+@Opcodes(PacketOpcodes.MusicGameStartToPlayOthersBeatmapReq)
+public class HandlerMusicGameStartToPlayOthersBeatmapReq extends PacketHandler {
+
+	@Override
+	public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
+        var req = MusicGameStartToPlayOthersBeatmapReqOuterClass.MusicGameStartToPlayOthersBeatmapReq.parseFrom(payload);
+
+		session.send(new PacketMusicGameStartToPlayOthersBeatmapRsp(req.getUnknownEnum1()));
+	}
+
+}
