a5b84ba86a37db312a6af782636018818b57e752
zhaodice <63996691+zhaodice@users.noreply.github.com>
2022-06-27 22:28:37 +0800

    add legacy function

--- src/main/java/emu/grasscutter/server/packet/send/PacketWidgetGadgetDataNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketWidgetGadgetDataNotify.java
@@ -9,6 +9,22 @@ import java.io.IOException;
 import java.util.List;
 
 public class PacketWidgetGadgetDataNotify extends BasePacket {
+    
+    public PacketWidgetGadgetDataNotify(int gadgetId, List<Integer> gadgetEntityIdList) throws IOException {
+        super(PacketOpcodes.WidgetGadgetDataNotify);
+
+        WidgetGadgetDataNotifyOuterClass.WidgetGadgetDataNotify proto = WidgetGadgetDataNotifyOuterClass.WidgetGadgetDataNotify.newBuilder()
+                .setWidgetGadgetData(
+                        WidgetGadgetDataOuterClass.WidgetGadgetData.newBuilder()
+                                .setGadgetId(gadgetId)
+                                .addAllGadgetEntityIdList(gadgetEntityIdList)
+                                .build()
+                )
+                .build();
+
+        this.setData(proto);
+    }
+    
     public PacketWidgetGadgetDataNotify(int gadgetId, int gadgetEntityIdList) throws IOException {
         super(PacketOpcodes.WidgetGadgetDataNotify);
 
