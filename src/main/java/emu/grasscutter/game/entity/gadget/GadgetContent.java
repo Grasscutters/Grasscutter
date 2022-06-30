d232df2ced8e9e256acacd9eb28304663462767c
zhaodice <63996691+zhaodice@users.noreply.github.com>
2022-06-30 21:25:25 +0800

    add default implement to GadgetBaseContent

--- src/main/java/emu/grasscutter/game/entity/gadget/GadgetContent.java
+++ src/main/java/emu/grasscutter/game/entity/gadget/GadgetContent.java
@@ -2,29 +2,27 @@ package emu.grasscutter.game.entity.gadget;
 
 import emu.grasscutter.game.entity.EntityGadget;
 import emu.grasscutter.game.player.Player;
-import emu.grasscutter.net.proto.GadgetInteractReqOuterClass.GadgetInteractReq;
-import emu.grasscutter.net.proto.SceneGadgetInfoOuterClass.SceneGadgetInfo;
+import emu.grasscutter.net.proto.GadgetInteractReqOuterClass;
+import emu.grasscutter.net.proto.SceneGadgetInfoOuterClass;
 import emu.grasscutter.net.proto.SelectWorktopOptionReqOuterClass;
 
-public class GadgetContent {
-	private final EntityGadget gadget;
-
-	public GadgetContent(EntityGadget gadget) {
-		this.gadget = gadget;
-	}
-
-	public EntityGadget getGadget() {
-		return gadget;
-	}
-
-	public boolean onInteract(Player player, GadgetInteractReq req) {
-	    return false;
+public class GadgetContent extends GadgetBaseContent {
+    public GadgetContent(EntityGadget gadget) {
+        super(gadget);
     }
 
+    @Override
+    public boolean onInteract(Player player, GadgetInteractReqOuterClass.GadgetInteractReq req) {
+        return false;
+    }
+
+    @Override
     public boolean onSelectWorktopOption(Player player, SelectWorktopOptionReqOuterClass.SelectWorktopOptionReq req) {
         return false;
     }
 
-	public void onBuildProto(SceneGadgetInfo.Builder gadgetInfo) {
+    @Override
+    public void onBuildProto(SceneGadgetInfoOuterClass.SceneGadgetInfo.Builder gadgetInfo) {
+
     }
 }
