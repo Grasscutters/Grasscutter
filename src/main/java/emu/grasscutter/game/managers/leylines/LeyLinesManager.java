6ccaee9857cb69b6c2f862f82db2af96bca467c0
zhaodice <63996691+zhaodice@users.noreply.github.com>
2022-07-02 21:36:44 +0800

    fix build

--- src/main/java/emu/grasscutter/game/managers/leylines/LeyLinesManager.java
+++ src/main/java/emu/grasscutter/game/managers/leylines/LeyLinesManager.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import emu.grasscutter.Grasscutter;
 import emu.grasscutter.game.entity.EntityGadget;
 import emu.grasscutter.game.entity.gadget.GadgetWorktop;
 import emu.grasscutter.game.inventory.GameItem;
 import emu.grasscutter.game.player.Player;
 import emu.grasscutter.game.world.Scene;
 import emu.grasscutter.net.proto.VisionTypeOuterClass;
+import emu.grasscutter.server.packet.send.PacketBlossomBriefInfoNotify;
 import emu.grasscutter.utils.Position;
 import emu.grasscutter.utils.Utils;
 
@@ -18,6 +20,7 @@ public class LeyLinesManager {
     private Player player;
     private final ArrayList<ActiveLeyLines> activeLeyLines = new ArrayList<>();
     private final ArrayList<ActiveLeyLines> activeChests = new ArrayList<>();
+    private final ArrayList<EntityGadget> createdEntity = new ArrayList<>();
     private static final HashMap<Integer,ArrayList<Reward>> REWARDS_GOLDEN = new HashMap<>();
     private static final HashMap<Integer,ArrayList<Reward>> REWARDS_BLUE = new HashMap<>();
     private static final HashMap<Integer,ArrayList<Integer>> monstersDifficulty = new HashMap<>();
@@ -64,6 +67,8 @@ public class LeyLinesManager {
         rewards.add(new Reward(105,20));
         rewards.add(new Reward(102,100));
         REWARDS_GOLDEN.put(6,rewards);
+        REWARDS_GOLDEN.put(7,rewards);
+        REWARDS_GOLDEN.put(8,rewards);
 
         //
         rewards = new ArrayList<>();
@@ -112,6 +117,8 @@ public class LeyLinesManager {
         rewards.add(new Reward(105,20));
         rewards.add(new Reward(102,100));
         REWARDS_BLUE.put(6,rewards);
+        REWARDS_BLUE.put(7,rewards);
+        REWARDS_BLUE.put(8,rewards);
     }
     static {
         monstersDifficulty.put(0,new ArrayList<>());
@@ -135,33 +142,7 @@ public class LeyLinesManager {
         registerMonster(20010301,0); // 风史莱姆
         registerMonster(20010401,0); // 大型风史莱姆
         registerMonster(20010403,0); // 大型风史莱姆
-        registerMonster(29010101,3); // 裂空的魔龙
-        registerMonster(29010102,3); // 裂空的魔龙
-        registerMonster(29010103,3); // 裂空的魔龙
-        registerMonster(29010104,3); // 裂空的魔龙
-        registerMonster(29020101,3); // 北风的王狼，奔狼的领主
-        registerMonster(29020102,3); // 北风的王狼，奔狼的领主
-        registerMonster(29030103,3); // 「公子」
-        registerMonster(29030106,3); // 「公子」
-        registerMonster(29040101,3); // 若陀龙王
-        registerMonster(29040102,3); // 若陀龙王
-        registerMonster(29040103,3); // 若陀龙王
-        registerMonster(29040104,3); // 若陀龙王
-        registerMonster(29040111,3); // 若陀龙王
-        registerMonster(29050102,3); // 「女士」
-        registerMonster(29050104,3); // 「女士」
-        registerMonster(29060202,3); // 祸津御建鸣神命
-        registerMonster(29060203,3); // 祸津御建鸣神命
-        registerMonster(20020101,1); // 狂风之核
-        registerMonster(20040101,2); // 无相之雷
-        registerMonster(20040102,2); // 无相之雷
-        registerMonster(20040201,2); // 无相之风
-        registerMonster(20040202,2); // 无相之风
-        registerMonster(20040301,2); // 无相之岩
-        registerMonster(20040302,2); // 无相之岩
-        registerMonster(20040401,2); // 无相之水
-        registerMonster(20040501,2); // 无相之冰
-        registerMonster(20040601,2); // 无相之火
+        registerMonster(20020101,2); // 狂风之核
         registerMonster(20010501,0); // 雷史莱姆
         registerMonster(20010601,0); // 大型雷史莱姆
         registerMonster(20010604,0); // 大型雷史莱姆
@@ -179,8 +160,6 @@ public class LeyLinesManager {
         registerMonster(20011701,0); // 无相之水·召唤物
         registerMonster(20011801,0); // 无相之水·召唤物
         registerMonster(20011901,0); // 无相之水·召唤物
-        registerMonster(20050101,3); // 纯水精灵
-        registerMonster(20050102,3); // 纯水精灵
         registerMonster(20050201,0); // 幻形豕兽·水
         registerMonster(20050202,0); // 幻形豕兽·水
         registerMonster(20050203,0); // 幻形豕兽·水
@@ -244,24 +223,8 @@ public class LeyLinesManager {
         registerMonster(22010401,2); // 雷深渊法师
         registerMonster(22010403,2); // 雷深渊法师
         registerMonster(22010404,2); // 雷深渊法师
-        registerMonster(22020101,2); // 深渊使徒·激流
-        registerMonster(22020102,2); // 深渊使徒·激流
-        registerMonster(22030101,2); // 深渊咏者·紫电
-        registerMonster(22030102,2); // 深渊咏者·紫电
-        registerMonster(22030201,2); // 深渊咏者·渊火
-        registerMonster(22030202,2); // 深渊咏者·渊火
-        registerMonster(22040101,2); // 嗜岩·兽境幼兽
-        registerMonster(22040201,2); // 嗜雷·兽境幼兽
-        registerMonster(22050101,2); // 嗜岩·兽境猎犬
-        registerMonster(22050201,2); // 嗜雷·兽境猎犬
-        registerMonster(22060101,2); // 黄金王兽
-        registerMonster(22070101,2); // 黯色空壳·旗令
-        registerMonster(22070102,2); // 黯色空壳·旗令
-        registerMonster(22070201,2); // 黯色空壳·破阵
-        registerMonster(22070202,2); // 黯色空壳·破阵
-        registerMonster(22070301,2); // 黯色空壳·近卫
-        registerMonster(22070302,2); // 黯色空壳·近卫
-        registerMonster(22080101,2); // 黑蛇骑士·斩风之剑
+
+
         registerMonster(21030101,0); // 水丘丘萨满
         registerMonster(21030103,0); // 水丘丘萨满
         registerMonster(21030201,0); // 草丘丘萨满
@@ -275,8 +238,6 @@ public class LeyLinesManager {
         registerMonster(21030601,0); // 雷丘丘萨满
         registerMonster(21030603,0); // 雷丘丘萨满
         registerMonster(24010101,2); // 遗迹守卫
-        registerMonster(24010108,2); // 遗迹守卫
-        registerMonster(24010109,2); // 遗迹守卫
         registerMonster(24010201,2); // 遗迹猎者
         registerMonster(24010301,2); // 遗迹重机
         registerMonster(24010303,2); // 遗迹重机
@@ -357,18 +318,11 @@ public class LeyLinesManager {
         registerMonster(25080301,0); // 野伏·机巧番
         registerMonster(25080401,0); // 寄骑武士
         registerMonster(25080402,0); // 寄骑武士
-        registerMonster(25090101,2); // 魔偶剑鬼
-        registerMonster(25090102,2); // 魔偶剑鬼
-        registerMonster(25090103,2); // 魔偶剑鬼
-        registerMonster(25090104,2); // 魔偶剑鬼
-        registerMonster(25090201,2); // 魔偶剑鬼·孤风
-        registerMonster(25090301,2); // 魔偶剑鬼·霜驰
-        registerMonster(25090401,2); // 魔偶剑鬼·凶面
         registerMonster(25100101,2); // 海乱鬼·雷腾
         registerMonster(25100301,0); // 落武者·咒雷
-        registerMonster(25100201,0); // 海乱鬼·炎威
+        registerMonster(25100201,2); // 海乱鬼·炎威
         registerMonster(25100401,0); // 落武者·祟炎
-        registerMonster(25100102,0); // 海乱鬼·雷腾
+        registerMonster(25100102,2); // 海乱鬼·雷腾
         registerMonster(25100302,0); // 落武者·咒雷
         registerMonster(26010101,0); // 冰霜骗骗花
         registerMonster(26010102,0); // 冰霜骗骗花
@@ -376,38 +330,21 @@ public class LeyLinesManager {
         registerMonster(26010104,0); // 冰霜骗骗花
         registerMonster(26010201,0); // 炽热骗骗花
         registerMonster(26010301,0); // 电气骗骗花
-        registerMonster(26020101,3); // 急冻树
-        registerMonster(26020102,3); // 急冻树
-        registerMonster(26020201,3); // 爆炎树
         registerMonster(26030101,1); // 幼岩龙蜥
         registerMonster(26040101,1); // 岩龙蜥
         registerMonster(26040102,1); // 岩龙蜥
         registerMonster(26040103,1); // 岩龙蜥
         registerMonster(26040104,1); // 岩龙蜥
-        registerMonster(26050101,2); // 古岩龙蜥
-        registerMonster(26050201,2); // 古岩龙蜥
-        registerMonster(26050301,2); // 古岩龙蜥
-        registerMonster(26050401,2); // 古岩龙蜥
-        registerMonster(26050501,2); // 古岩龙蜥
-        registerMonster(26050601,2); // 深海龙蜥·原种
-        registerMonster(26050901,2); // 深海龙蜥·原种
-        registerMonster(26050701,2); // 深海龙蜥·啮冰
-        registerMonster(26051001,2); // 深海龙蜥·啮冰
-        registerMonster(26050702,2); // 深海龙蜥·啮冰
-        registerMonster(26050801,2); // 深海龙蜥·吞雷
-        registerMonster(26051101,2); // 深海龙蜥·吞雷
-        registerMonster(26050802,2); // 深海龙蜥·吞雷
         registerMonster(26060101,0); // 雷萤
         registerMonster(26060201,0); // 水萤
         registerMonster(26060301,0); // 冰萤
-        registerMonster(26080101,0); // 跋掣
         registerMonster(26090101,0); // 浮游水蕈兽
-        registerMonster(20060101,0); // 水飘浮灵
-        registerMonster(20060201,0); // 岩飘浮灵
-        registerMonster(20060301,0); // 风飘浮灵
-        registerMonster(20060401,0); // 冰飘浮灵
-        registerMonster(20060501,0); // 雷飘浮灵
-        registerMonster(20060601,0); // 火飘浮灵
+        registerMonster(20060101,2); // 水飘浮灵
+        registerMonster(20060201,2); // 岩飘浮灵
+        registerMonster(20060301,2); // 风飘浮灵
+        registerMonster(20060401,2); // 冰飘浮灵
+        registerMonster(20060501,2); // 雷飘浮灵
+        registerMonster(20060601,2); // 火飘浮灵
         registerMonster(21010102,0); // 丘丘人
         registerMonster(21020102,1); // 木盾丘丘暴徒
         registerMonster(21020203,1); // 火斧丘丘暴徒
@@ -444,10 +381,88 @@ public class LeyLinesManager {
         registerMonster(21030403,0); // 岩丘丘萨满
         registerMonster(21030502,0); // 冰丘丘萨满
         registerMonster(21030602,0); // 雷丘丘萨满
+        registerMonster(26040105,1); // 岩龙蜥
+
+        /*
+
+        Super Monsters
+
         registerMonster(24010102,2); // 遗迹守卫
         registerMonster(24010202,2); // 遗迹猎者
         registerMonster(24010302,2); // 遗迹重机
-        registerMonster(26040105,1); // 岩龙蜥
+        registerMonster(26050601,2); // 深海龙蜥·原种
+        registerMonster(26050901,2); // 深海龙蜥·原种
+        registerMonster(26050701,2); // 深海龙蜥·啮冰
+        registerMonster(26051001,2); // 深海龙蜥·啮冰
+        registerMonster(26050702,2); // 深海龙蜥·啮冰
+        registerMonster(26050801,2); // 深海龙蜥·吞雷
+        registerMonster(26051101,2); // 深海龙蜥·吞雷
+        registerMonster(26050802,2); // 深海龙蜥·吞雷
+
+        registerMonster(26050101,2); // 古岩龙蜥
+        registerMonster(26050201,2); // 古岩龙蜥
+        registerMonster(26050301,2); // 古岩龙蜥
+        registerMonster(26050401,2); // 古岩龙蜥
+        registerMonster(26050501,2); // 古岩龙蜥
+        registerMonster(26080101,3); // 跋掣
+        registerMonster(25090101,2); // 魔偶剑鬼
+        registerMonster(25090102,2); // 魔偶剑鬼
+        registerMonster(25090103,2); // 魔偶剑鬼
+        registerMonster(25090104,2); // 魔偶剑鬼
+        registerMonster(25090201,2); // 魔偶剑鬼·孤风
+        registerMonster(25090301,2); // 魔偶剑鬼·霜驰
+        registerMonster(25090401,2); // 魔偶剑鬼·凶面
+        registerMonster(26020101,3); // 急冻树
+        registerMonster(26020102,3); // 急冻树
+        registerMonster(26020201,3); // 爆炎树
+        registerMonster(29010101,3); // 裂空的魔龙
+        registerMonster(29010102,3); // 裂空的魔龙
+        registerMonster(29010103,3); // 裂空的魔龙
+        registerMonster(29010104,3); // 裂空的魔龙
+        registerMonster(29020101,3); // 北风的王狼，奔狼的领主
+        registerMonster(29020102,3); // 北风的王狼，奔狼的领主
+        registerMonster(29030103,3); // 「公子」
+        registerMonster(29030106,3); // 「公子」
+        registerMonster(29040101,3); // 若陀龙王
+        registerMonster(29040102,3); // 若陀龙王
+        registerMonster(29040103,3); // 若陀龙王
+        registerMonster(29040104,3); // 若陀龙王
+        registerMonster(29040111,3); // 若陀龙王
+        registerMonster(29050102,3); // 「女士」
+        registerMonster(29050104,3); // 「女士」
+        registerMonster(29060202,3); // 祸津御建鸣神命
+        registerMonster(29060203,3); // 祸津御建鸣神命
+        registerMonster(20040101,2); // 无相之雷
+        registerMonster(20040102,2); // 无相之雷
+        registerMonster(20040201,2); // 无相之风
+        registerMonster(20040202,2); // 无相之风
+        registerMonster(20040301,2); // 无相之岩
+        registerMonster(20040302,2); // 无相之岩
+        registerMonster(20040401,2); // 无相之水
+        registerMonster(20040501,2); // 无相之冰
+        registerMonster(20040601,2); // 无相之火
+        registerMonster(20050101,3); // 纯水精灵
+        registerMonster(20050102,3); // 纯水精灵
+        registerMonster(22020101,2); // 深渊使徒·激流
+        registerMonster(22020102,2); // 深渊使徒·激流
+        registerMonster(22030101,2); // 深渊咏者·紫电
+        registerMonster(22030102,2); // 深渊咏者·紫电
+        registerMonster(22030201,2); // 深渊咏者·渊火
+        registerMonster(22030202,2); // 深渊咏者·渊火
+        registerMonster(22040101,2); // 嗜岩·兽境幼兽
+        registerMonster(22040201,2); // 嗜雷·兽境幼兽
+        registerMonster(22050101,2); // 嗜岩·兽境猎犬
+        registerMonster(22050201,2); // 嗜雷·兽境猎犬
+        registerMonster(22060101,2); // 黄金王兽
+        registerMonster(22070101,2); // 黯色空壳·旗令
+        registerMonster(22070102,2); // 黯色空壳·旗令
+        registerMonster(22070201,2); // 黯色空壳·破阵
+        registerMonster(22070202,2); // 黯色空壳·破阵
+        registerMonster(22070301,2); // 黯色空壳·近卫
+        registerMonster(22070302,2); // 黯色空壳·近卫
+        registerMonster(22080101,2); // 黑蛇骑士·斩风之剑
+
+         */
     }
     private static void registerMonster(int monsterId,int difficulty){
         monstersDifficulty.get(difficulty).add(monsterId);
@@ -469,6 +484,10 @@ public class LeyLinesManager {
             }
         }
     }
+    public void recycleLeyLineGadgetEntity(EntityGadget entityGadget){
+        createdEntity.remove(entityGadget);
+        player.getScene().broadcastPacket(new PacketBlossomBriefInfoNotify(createdEntity));
+    }
     public EntityGadget createLeyLineGadgetEntity(Position pos, LeyLinesType leyLinesType){
         int worldLevel = getWorldLevel();
         Scene scene = getScene();
@@ -486,9 +505,14 @@ public class LeyLinesManager {
                         return false;
                     }
                 }
-                var monsters = getRandomMonstersID(0,4);
+                ArrayList<Integer> monsters;
+                //switch(Utils.randomRange(0,4)) {
+
+                //}
+                monsters = getRandomMonstersID(0,4);
                 monsters.addAll(getRandomMonstersID(1,2));
                 monsters.addAll(getRandomMonstersID(2,1));
+                System.out.println("LeyLine Monsters:"+monsters);
                 leyLine = new ActiveLeyLines(entityGadget, monsters, -1, worldLevel);
                 activeLeyLines.add(leyLine);
             }
@@ -498,6 +522,8 @@ public class LeyLinesManager {
             leyLine.start();
             return true;
         });
+        createdEntity.add(newGadget);
+        player.getScene().broadcastPacket(new PacketBlossomBriefInfoNotify(createdEntity));
         return newGadget;
         //player.getScene().addEntity(entityGadget);
     }
@@ -527,10 +553,14 @@ public class LeyLinesManager {
                         } else {
                             rewards = REWARDS_GOLDEN.get(worldLevel);
                         }
+                        if(rewards==null){
+                            Grasscutter.getLogger().error("Leyline could not support world level : "+worldLevel);
+                        }
                         for (Reward reward : rewards) {
                             items.add(new GameItem(reward.itemId, Utils.randomRange(reward.minCount, reward.maxCount)));
                         }
                         it.remove();
+                        recycleLeyLineGadgetEntity(activeChest.getGadget());
                         return items;
                     }
                     return null;
