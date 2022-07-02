a80302cdcd41da9494bc30546bf5881b3a3457e5
AnimeGitB <AnimeGitB@bigblueball.in>
2022-07-02 18:08:41 +0930

    Fix commands in party chat, re-add optional positional item quantity to give command, error out and print usage if excess arguments given to give command

--- src/main/java/emu/grasscutter/game/managers/chat/ChatManager.java
+++ src/main/java/emu/grasscutter/game/managers/chat/ChatManager.java
@@ -7,14 +7,15 @@ import emu.grasscutter.server.game.GameServer;
 import emu.grasscutter.server.packet.send.PacketPlayerChatNotify;
 import emu.grasscutter.server.packet.send.PacketPrivateChatNotify;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class ChatManager implements ChatManagerHandler {
-	static final List<Character> PREFIXES = Arrays.asList('/', '!');
-	
+	static final String PREFIXES = "[/!]";
+	static final Pattern RE_PREFIXES = Pattern.compile(PREFIXES);
+	static final Pattern RE_COMMANDS = Pattern.compile("\n" + PREFIXES);
+
 	private final GameServer server;
-	
+
 	public ChatManager(GameServer server) {
 		this.server = server;
 	}
@@ -23,56 +24,62 @@ public class ChatManager implements ChatManagerHandler {
 		return server;
 	}
 
+	private boolean tryInvokeCommand(Player sender, Player target, String rawMessage) {
+		if (!RE_PREFIXES.matcher(rawMessage.substring(0, 1)).matches())
+			return false;
+		for (String line : rawMessage.substring(1).split("\n[/!]"))
+			CommandMap.getInstance().invoke(sender, target, line);
+		return true;
+	}
+
 	public void sendPrivateMessage(Player player, int targetUid, String message) {
 		// Sanity checks
 		if (message == null || message.length() == 0) {
 			return;
 		}
-		
+
 		// Get target
 		Player target = getServer().getPlayerByUid(targetUid);
-				
+
 		// Check if command
-		if (PREFIXES.contains(message.charAt(0))) {
-			CommandMap.getInstance().invoke(player, target, message.substring(1));
+		if (tryInvokeCommand(player, target, message)) {
 			return;
 		}
-		
+
 		if (target == null) {
 			return;
 		}
-		
+
 		// Create chat packet
 		BasePacket packet = new PacketPrivateChatNotify(player.getUid(), target.getUid(), message);
-		
+
 		player.sendPacket(packet);
 		target.sendPacket(packet);
 	}
-	
+
 	public void sendPrivateMessage(Player player, int targetUid, int emote) {
 		// Get target
 		Player target = getServer().getPlayerByUid(targetUid);
-		
+
 		if (target == null) {
 			return;
 		}
-		
+
 		// Create chat packet
 		BasePacket packet = new PacketPrivateChatNotify(player.getUid(), target.getUid(), emote);
-		
+
 		player.sendPacket(packet);
 		target.sendPacket(packet);
 	}
-	
+
 	public void sendTeamMessage(Player player, int channel, String message) {
 		// Sanity checks
 		if (message == null || message.length() == 0) {
 			return;
 		}
-				
+
 		// Check if command
-		if (PREFIXES.contains(message.charAt(0))) {
-			CommandMap.getInstance().invoke(player, null, message);
+		if (tryInvokeCommand(player, null, message)) {
 			return;
 		}
 
