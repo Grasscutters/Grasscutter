0031da02c3fd795b7755331a7e675078b4fc710c
AnimeGitB <AnimeGitB@bigblueball.in>
2022-07-04 01:27:50 +0930

    Add gachaTimesLimit for beginner banner Also add some more return codes

--- src/main/java/emu/grasscutter/game/gacha/GachaBanner.java
+++ src/main/java/emu/grasscutter/game/gacha/GachaBanner.java
@@ -1,77 +1,51 @@
 package emu.grasscutter.game.gacha;
 
-import emu.grasscutter.database.DatabaseHelper;
-import emu.grasscutter.game.Account;
+import emu.grasscutter.data.common.ItemParamData;
 import emu.grasscutter.game.player.Player;
 import emu.grasscutter.net.proto.GachaInfoOuterClass.GachaInfo;
 import emu.grasscutter.net.proto.GachaUpInfoOuterClass.GachaUpInfo;
 import emu.grasscutter.utils.Utils;
+import lombok.Getter;
 
 import static emu.grasscutter.Configuration.*;
 
-import emu.grasscutter.Grasscutter;
-import emu.grasscutter.data.common.ItemParamData;
-
 public class GachaBanner {
-	private int gachaType;
-	private int scheduleId;
-	private String prefabPath;
-	private String previewPrefabPath;
-	private String titlePath;
+	@Getter private int gachaType;
+	@Getter private int scheduleId;
+	@Getter private String prefabPath;
+	@Getter private String previewPrefabPath;
+	@Getter private String titlePath;
 	private int costItemId = 0;
 	private int costItemAmount = 1;
 	private int costItemId10 = 0;
 	private int costItemAmount10 = 10;
-	private int beginTime;
-	private int endTime;
-	private int sortId;
+	@Getter private int beginTime;
+	@Getter private int endTime;
+	@Getter private int sortId;
+	@Getter private int gachaTimesLimit = Integer.MAX_VALUE;
 	private int[] rateUpItems4 = {};
 	private int[] rateUpItems5 = {};
-	private int[] fallbackItems3 = {11301, 11302, 11306, 12301, 12302, 12305, 13303, 14301, 14302, 14304, 15301, 15302, 15304};
-	private int[] fallbackItems4Pool1 = {1014, 1020, 1023, 1024, 1025, 1027, 1031, 1032, 1034, 1036, 1039, 1043, 1044, 1045, 1048, 1053, 1055, 1056, 1064};
-	private int[] fallbackItems4Pool2 = {11401, 11402, 11403, 11405, 12401, 12402, 12403, 12405, 13401, 13407, 14401, 14402, 14403, 14409, 15401, 15402, 15403, 15405};
-	private int[] fallbackItems5Pool1 = {1003, 1016, 1042, 1035, 1041};
-	private int[] fallbackItems5Pool2 = {11501, 11502, 12501, 12502, 13502, 13505, 14501, 14502, 15501, 15502};
-	private boolean removeC6FromPool = false;
-	private boolean autoStripRateUpFromFallback = true;
+	@Getter private int[] fallbackItems3 = {11301, 11302, 11306, 12301, 12302, 12305, 13303, 14301, 14302, 14304, 15301, 15302, 15304};
+	@Getter private int[] fallbackItems4Pool1 = {1014, 1020, 1023, 1024, 1025, 1027, 1031, 1032, 1034, 1036, 1039, 1043, 1044, 1045, 1048, 1053, 1055, 1056, 1064};
+	@Getter private int[] fallbackItems4Pool2 = {11401, 11402, 11403, 11405, 12401, 12402, 12403, 12405, 13401, 13407, 14401, 14402, 14403, 14409, 15401, 15402, 15403, 15405};
+	@Getter private int[] fallbackItems5Pool1 = {1003, 1016, 1042, 1035, 1041};
+	@Getter private int[] fallbackItems5Pool2 = {11501, 11502, 12501, 12502, 13502, 13505, 14501, 14502, 15501, 15502};
+	@Getter private boolean removeC6FromPool = false;
+	@Getter private boolean autoStripRateUpFromFallback = true;
 	private int[][] weights4 = {{1,510}, {8,510}, {10,10000}};
 	private int[][] weights5 = {{1,75}, {73,150}, {90,10000}};
 	private int[][] poolBalanceWeights4 = {{1,255}, {17,255}, {21,10455}};
 	private int[][] poolBalanceWeights5 = {{1,30}, {147,150}, {181,10230}};
 	private int eventChance4 = 50; // Chance to win a featured event item
 	private int eventChance5 = 50; // Chance to win a featured event item
-	private BannerType bannerType = BannerType.STANDARD;
+	@Getter private BannerType bannerType = BannerType.STANDARD;
 
 	// Kinda wanna deprecate these but they're in people's configs
 	private int[] rateUpItems1 = {};
 	private int[] rateUpItems2 = {};
 	private int eventChance = -1;
 	private int costItem = 0;
-	private int wishMaxProgress = 2;
-	
-	public int getGachaType() {
-		return gachaType;
-	}
-
-	public BannerType getBannerType() {
-		return bannerType;
-	}
-
-	public int getScheduleId() {
-		return scheduleId;
-	}
-
-	public String getPrefabPath() {
-		return prefabPath;
-	}
-
-	public String getPreviewPrefabPath() {
-		return previewPrefabPath;
-	}
-
-	public String getTitlePath() {
-		return titlePath;
-	}
+	@Getter private int wishMaxProgress = 2;
 
 	public ItemParamData getCost(int numRolls) {
 		return switch (numRolls) {
@@ -84,35 +58,12 @@ public class GachaBanner {
 		return (costItem > 0) ? costItem : costItemId;
 	}
 
-	public int getBeginTime() {
-		return beginTime;
-	}
-
-	public int getEndTime() {
-		return endTime;
-	}
-
-	public int getSortId() {
-		return sortId;
-	}
-
 	public int[] getRateUpItems4() {
 		return (rateUpItems2.length > 0) ? rateUpItems2 : rateUpItems4;
 	}
 	public int[] getRateUpItems5() {
 		return (rateUpItems1.length > 0) ? rateUpItems1 : rateUpItems5;
 	}
-
-	public int[] getFallbackItems3() {return fallbackItems3;}
-	public int[] getFallbackItems4Pool1() {return fallbackItems4Pool1;}
-	public int[] getFallbackItems4Pool2() {return fallbackItems4Pool2;}
-	public int[] getFallbackItems5Pool1() {return fallbackItems5Pool1;}
-	public int[] getFallbackItems5Pool2() {return fallbackItems5Pool2;}
-
-	public boolean getRemoveC6FromPool() {return removeC6FromPool;}
-	public boolean getAutoStripRateUpFromFallback() {return autoStripRateUpFromFallback;}
-
-	public int getWishMaxProgress() {return wishMaxProgress;}
 
 	public boolean hasEpitomized() {
 		return bannerType.equals(BannerType.WEAPON);
@@ -155,6 +106,11 @@ public class GachaBanner {
 		// Grasscutter.getLogger().info("record = " + record);
 		ItemParamData costItem1 = this.getCost(1);
 		ItemParamData costItem10 = this.getCost(10);
+		PlayerGachaBannerInfo gachaInfo = player.getGachaInfo().getBannerInfo(this);
+		int leftGachaTimes = switch(gachaTimesLimit) {
+			case Integer.MAX_VALUE -> Integer.MAX_VALUE;
+			default -> Math.max(gachaTimesLimit - gachaInfo.getTotalPulls(), 0);
+		};
 		GachaInfo.Builder info = GachaInfo.newBuilder()
 				.setGachaType(this.getGachaType())
 				.setScheduleId(this.getScheduleId())
@@ -170,13 +126,11 @@ public class GachaBanner {
 	            .setGachaProbUrlOversea(details)
 	            .setGachaRecordUrl(record)
 	            .setGachaRecordUrlOversea(record)
-	            .setLeftGachaTimes(Integer.MAX_VALUE)
-	            .setGachaTimesLimit(Integer.MAX_VALUE)
+	            .setLeftGachaTimes(leftGachaTimes)
+	            .setGachaTimesLimit(gachaTimesLimit)
 	            .setGachaSortId(this.getSortId());
 
 		if(hasEpitomized()) {
-			PlayerGachaBannerInfo gachaInfo = player.getGachaInfo().getBannerInfo(this);
-
 			info.setWishItemId(gachaInfo.getWishItemId())
 				.setWishProgress(gachaInfo.getFailedChosenItemPulls())
 				.setWishMaxProgress(this.getWishMaxProgress());
