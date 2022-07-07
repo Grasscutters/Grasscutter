47980028d142eeec181117d1f5bf8ded5130e374
shädam <47268949+supahero1@users.noreply.github.com>
2022-07-07 23:00:22 +0200

    Update SendMailCommand.java

--- src/main/java/emu/grasscutter/command/commands/SendMailCommand.java
+++ src/main/java/emu/grasscutter/command/commands/SendMailCommand.java
@@ -13,7 +13,13 @@ import java.util.List;
 import static emu.grasscutter.utils.Language.translate;
 
 @SuppressWarnings("ConstantConditions")
-@Command(label = "sendmail", usage = "sendmail <userId|all|help> [templateId]", permission = "server.sendmail", description = "commands.sendMail.description", targetRequirement = Command.TargetRequirement.NONE)
+@Command(
+    label = "sendmail",
+    usage = "commands.sendMail.usage",
+    description = "commands.sendMail.description",
+    targetRequirement = Command.TargetRequirement.NONE,
+    permission = "server.sendmail"
+)
 public final class SendMailCommand implements CommandHandler {
 
     // TODO: You should be able to do /sendmail and then just send subsequent messages until you finish
