dc2a434ee15929257857e3a19b832ae8b26675ff
KingRainbow44 <kobedo11@gmail.com>
2022-07-15 12:43:49 -0400

    Implement `PlayerMoveEvent`

--- src/main/java/emu/grasscutter/game/entity/GameEntity.java
+++ src/main/java/emu/grasscutter/game/entity/GameEntity.java
@@ -24,32 +24,32 @@ public abstract class GameEntity {
 	protected int id;
 	private final Scene scene;
 	private SpawnDataEntry spawnEntry;
-	
+
 	private int blockId;
 	private int configId;
 	private int groupId;
-	
+
 	private MotionState moveState;
 	private int lastMoveSceneTimeMs;
 	private int lastMoveReliableSeq;
-	
+
 	// Abilities
 	private Map<String, Float> metaOverrideMap;
 	private Int2ObjectMap<String> metaModifiers;
-	
+
 	public GameEntity(Scene scene) {
 		this.scene = scene;
 		this.moveState = MotionState.MOTION_STATE_NONE;
 	}
-	
+
 	public int getId() {
 		return this.id;
 	}
-	
+
 	public int getEntityType() {
 		return getId() >> 24;
 	}
-	
+
 	public World getWorld() {
 		return this.getScene().getWorld();
 	}
@@ -57,7 +57,7 @@ public abstract class GameEntity {
 	public Scene getScene() {
 		return this.scene;
 	}
-	
+
 	public boolean isAlive() {
 		return true;
 	}
@@ -65,14 +65,14 @@ public abstract class GameEntity {
 	public LifeState getLifeState() {
 		return isAlive() ? LifeState.LIFE_ALIVE : LifeState.LIFE_DEAD;
 	}
-	
+
 	public Map<String, Float> getMetaOverrideMap() {
 		if (this.metaOverrideMap == null) {
 			this.metaOverrideMap = new HashMap<>();
 		}
 		return this.metaOverrideMap;
 	}
-	
+
 	public Int2ObjectMap<String> getMetaModifiers() {
 		if (this.metaModifiers == null) {
 			this.metaModifiers = new Int2ObjectOpenHashMap<>();
@@ -81,11 +81,11 @@ public abstract class GameEntity {
 	}
 
 	public abstract Int2FloatOpenHashMap getFightProperties();
-	
+
 	public abstract Position getPosition();
-	
+
 	public abstract Position getRotation();
-	
+
 	public MotionState getMotionState() {
 		return moveState;
 	}
@@ -109,23 +109,23 @@ public abstract class GameEntity {
 	public void setLastMoveReliableSeq(int lastMoveReliableSeq) {
 		this.lastMoveReliableSeq = lastMoveReliableSeq;
 	}
-	
+
 	public void setFightProperty(FightProperty prop, float value) {
 		this.getFightProperties().put(prop.getId(), value);
 	}
-	
+
 	private void setFightProperty(int id, float value) {
 		this.getFightProperties().put(id, value);
 	}
-	
+
 	public void addFightProperty(FightProperty prop, float value) {
 		this.getFightProperties().put(prop.getId(), getFightProperty(prop) + value);
 	}
-	
+
 	public float getFightProperty(FightProperty prop) {
 		return getFightProperties().getOrDefault(prop.getId(), 0f);
 	}
-	
+
 	public void addAllFightPropsToEntityInfo(SceneEntityInfo.Builder entityInfo) {
 		for (Int2FloatMap.Entry entry : getFightProperties().int2FloatEntrySet()) {
 			if (entry.getIntKey() == 0) {
@@ -135,7 +135,7 @@ public abstract class GameEntity {
 			entityInfo.addFightPropList(fightProp);
 		}
 	}
-	
+
 	public int getBlockId() {
 		return blockId;
 	}
@@ -143,7 +143,7 @@ public abstract class GameEntity {
 	public void setBlockId(int blockId) {
 		this.blockId = blockId;
 	}
-	
+
 	public int getConfigId() {
 		return configId;
 	}
@@ -151,7 +151,7 @@ public abstract class GameEntity {
 	public void setConfigId(int configId) {
 		this.configId = configId;
 	}
-	
+
 	public int getGroupId() {
 		return groupId;
 	}
@@ -159,7 +159,7 @@ public abstract class GameEntity {
 	public void setGroupId(int groupId) {
 		this.groupId = groupId;
 	}
-	
+
 	protected MotionInfo getMotionInfo() {
 		MotionInfo proto = MotionInfo.newBuilder()
 				.setPos(getPosition().toProto())
@@ -167,7 +167,7 @@ public abstract class GameEntity {
 				.setSpeed(Vector.newBuilder())
 				.setState(this.getMotionState())
 				.build();
-		
+
 		return proto;
 	}
 
@@ -178,7 +178,7 @@ public abstract class GameEntity {
 	public void setSpawnEntry(SpawnDataEntry spawnEntry) {
 		this.spawnEntry = spawnEntry;
 	}
-	
+
 	public float heal(float amount) {
 		if (this.getFightProperties() == null) {
 			return 0f;
@@ -186,62 +186,73 @@ public abstract class GameEntity {
 
 		float curHp = getFightProperty(FightProperty.FIGHT_PROP_CUR_HP);
 		float maxHp = getFightProperty(FightProperty.FIGHT_PROP_MAX_HP);
-		
+
 		if (curHp >= maxHp) {
 			return 0f;
 		}
-		
+
 		float healed = Math.min(maxHp - curHp, amount);
 		this.addFightProperty(FightProperty.FIGHT_PROP_CUR_HP, healed);
-		
+
 		getScene().broadcastPacket(new PacketEntityFightPropUpdateNotify(this, FightProperty.FIGHT_PROP_CUR_HP));
-		
+
 		return healed;
 	}
-	
+
 	public void damage(float amount) {
 		damage(amount, 0);
 	}
-	
+
 	public void damage(float amount, int killerId) {
 		// Sanity check
 		if (getFightProperties() == null) {
 			return;
 		}
-		
+
 		// Lose hp
 		addFightProperty(FightProperty.FIGHT_PROP_CUR_HP, -amount);
-		
+
 		// Check if dead
 		boolean isDead = false;
 		if (getFightProperty(FightProperty.FIGHT_PROP_CUR_HP) <= 0f) {
 			setFightProperty(FightProperty.FIGHT_PROP_CUR_HP, 0f);
 			isDead = true;
 		}
-		
+
 		// Packets
 		this.getScene().broadcastPacket(new PacketEntityFightPropUpdateNotify(this, FightProperty.FIGHT_PROP_CUR_HP));
-		
+
 		// Check if dead
 		if (isDead) {
 			getScene().killEntity(this, killerId);
 		}
 	}
-	
+
+    /**
+     * Move this entity to a new position.
+     * @param position The new position.
+     * @param rotation The new rotation.
+     */
+    public void move(Position position, Position rotation) {
+        // Set the position and rotation.
+        this.getPosition().set(position);
+        this.getRotation().set(rotation);
+    }
+
     /**
      * Called when this entity is added to the world
      */
 	public void onCreate() {
-		
+
 	}
-	
+
 	/**
      * Called when this entity dies
      * @param killerId Entity id of the entity that killed this entity
      */
 	public void onDeath(int killerId) {
-		
+
 	}
-	
+
 	public abstract SceneEntityInfo toProto();
 }
