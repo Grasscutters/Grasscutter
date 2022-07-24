d6e6aa4c060746fc78d6557d398a2f35a66b9a53
GanyusLeftHorn <1244229+GanyusLeftHorn@users.noreply.github.com>
2022-07-24 09:12:07 +0200

    Read Open States from Excels (#1557)
    * Make sure we never access PlayerOpenStateManager::map directly.
    
    * Read OpenStates from excels.
    
    * Add defaultState
    
    * Replace hardcoded open states with the ones read from excels.
    
    * Don't send change notify when unlocking on login.
    
    * Add open state blacklist for default unlocks.
    
    * Add a way to temporarily set open states for dev
    
    * Remove old OpenState.java
    
    * Fix UnlockAllCommand
    
    * Change condType to an enum.

--- src/main/java/emu/grasscutter/command/commands/UnlockAllCommand.java
+++ src/main/java/emu/grasscutter/command/commands/UnlockAllCommand.java
@@ -2,8 +2,9 @@ package emu.grasscutter.command.commands;
 
 import emu.grasscutter.command.Command;
 import emu.grasscutter.command.CommandHandler;
+import emu.grasscutter.data.GameData;
 import emu.grasscutter.game.player.Player;
-import emu.grasscutter.game.props.OpenState;
+import emu.grasscutter.game.player.PlayerOpenStateManager;
 import emu.grasscutter.server.packet.send.PacketOpenStateChangeNotify;
 
 import java.util.HashMap;
@@ -18,13 +19,16 @@ public final class UnlockAllCommand implements CommandHandler {
     @Override
     public void execute(Player sender, Player targetPlayer, List<String> args) {
         Map<Integer, Integer> changed = new HashMap<>();
-        
-        for (OpenState state : OpenState.values()) {
-            if (state == OpenState.OPEN_STATE_NONE || state == OpenState.OPEN_STATE_LIMIT_REGION_GLOBAL) continue;
-            
-            if (targetPlayer.getOpenStateManager().getOpenStateMap().getOrDefault(state.getValue(), 0) == 0) {
-                targetPlayer.getOpenStateManager().getOpenStateMap().put(state.getValue(), 1); 
-                changed.put(state.getValue(), 1);
+
+        for (var state : GameData.getOpenStateList()) {
+            // Don't unlock blacklisted open states.
+            if (PlayerOpenStateManager.BLACKLIST_OPEN_STATES.contains(state.getId())) {
+                continue;
+            }
+
+            if (targetPlayer.getOpenStateManager().getOpenState(state.getId()) == 0) {
+                targetPlayer.getOpenStateManager().getOpenStateMap().put(state.getId(), 1); 
+                changed.put(state.getId(), 1);
             }
         }
         
