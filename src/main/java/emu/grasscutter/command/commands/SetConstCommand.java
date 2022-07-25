4f23ea726d8dd805a80fd560463f08a8afe86082
juuyokka <15185244+juuyokka@users.noreply.github.com>
2022-07-24 21:03:04 -0700

    implemented `SetConstCommand`

--- src/main/java/emu/grasscutter/command/commands/SetConstCommand.java
+++ src/main/java/emu/grasscutter/command/commands/SetConstCommand.java
@@ -7,8 +7,7 @@ import emu.grasscutter.game.entity.EntityAvatar;
 import emu.grasscutter.game.player.Player;
 
 import java.util.List;
-
-import static emu.grasscutter.utils.Language.translate;
+import java.util.Set;
 
 @Command(
     label = "setConst",
@@ -19,11 +18,38 @@ import static emu.grasscutter.utils.Language.translate;
 public final class SetConstCommand implements CommandHandler {
     @Override
     public void execute(Player sender, Player targetPlayer, List<String> args) {
-        if (args.size() != 1) {
+        if (args.size() < 1) {
             sendUsageMessage(sender);
             return;
         }
 
-        CommandHandler.sendMessage(sender, args.get(0));
+        try {
+            int constLevel = Integer.parseInt(args.get(0));
+            if (constLevel < 0 || constLevel > 6) {
+                CommandHandler.sendTranslatedMessage(sender, "commands.setConst.range_error");
+                return;
+            }
+
+            EntityAvatar entity = targetPlayer.getTeamManager().getCurrentAvatarEntity();
+            if (entity == null) return;
+            Avatar avatar = entity.getAvatar();
+            this.setConstellation(avatar, constLevel);
+
+            CommandHandler.sendTranslatedMessage(sender, "commands.setConst.success", avatar.getAvatarData().getName(), constLevel);
+        } catch (NumberFormatException ignored) {
+            CommandHandler.sendTranslatedMessage(sender, "commands.setConst.level_error");
+        }
+    }
+
+    private void setConstellation(Avatar avatar, int constLevel) {
+        List<Integer> talentIds = avatar.getSkillDepot().getTalents();
+        Set<Integer> talentIdList = avatar.getTalentIdList();
+
+        talentIdList.clear();
+        talentIdList.addAll(talentIds.stream().limit(constLevel).toList());
+
+        avatar.setCoreProudSkillLevel(constLevel);
+        avatar.recalcStats();
+        avatar.save();
     }
 }
