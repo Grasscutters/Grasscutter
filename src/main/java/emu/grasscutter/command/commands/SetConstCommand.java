ac32fd9234422fcc5aa7dbd3dc99c71e143b7298
juuyokka <15185244+juuyokka@users.noreply.github.com>
2022-07-24 18:08:15 -0700

    (partially) functional `setconst` command

--- src/main/java/emu/grasscutter/command/commands/SetConstCommand.java
+++ src/main/java/emu/grasscutter/command/commands/SetConstCommand.java
@@ -6,13 +6,24 @@ import emu.grasscutter.game.avatar.Avatar;
 import emu.grasscutter.game.entity.EntityAvatar;
 import emu.grasscutter.game.player.Player;
 
+import java.util.List;
+
 import static emu.grasscutter.utils.Language.translate;
 
 @Command(
     label = "setConst",
     aliases = {"setconstellation"},
     usage = "<constellation level>",
-    permission = "player.setconstellation")
+    permission = "player.setconstellation",
+    permissionTargeted = "player.setconstellation.others")
 public final class SetConstCommand implements CommandHandler {
+    @Override
+    public void execute(Player sender, Player targetPlayer, List<String> args) {
+        if (args.size() != 1) {
+            sendUsageMessage(sender);
+            return;
+        }
 
+        CommandHandler.sendMessage(sender, args.get(0));
+    }
 }
