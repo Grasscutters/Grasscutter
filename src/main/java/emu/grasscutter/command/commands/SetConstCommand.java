6432fef1bc926d53dd35467eb361d856a266995d
juuyokka <15185244+juuyokka@users.noreply.github.com>
2022-07-25 18:45:37 -0700

    refactor + automatic reload

--- src/main/java/emu/grasscutter/command/commands/SetConstCommand.java
+++ src/main/java/emu/grasscutter/command/commands/SetConstCommand.java
@@ -1,10 +1,18 @@
 package emu.grasscutter.command.commands;
 
+import emu.grasscutter.Grasscutter;
 import emu.grasscutter.command.Command;
 import emu.grasscutter.command.CommandHandler;
+import emu.grasscutter.data.GameData;
+import emu.grasscutter.data.excels.AvatarTalentData;
 import emu.grasscutter.game.avatar.Avatar;
 import emu.grasscutter.game.entity.EntityAvatar;
 import emu.grasscutter.game.player.Player;
+import emu.grasscutter.game.world.Scene;
+import emu.grasscutter.game.world.World;
+import emu.grasscutter.server.packet.send.*;
+import emu.grasscutter.utils.Position;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 
 import java.util.List;
 import java.util.Set;
@@ -33,7 +41,8 @@ public final class SetConstCommand implements CommandHandler {
             EntityAvatar entity = targetPlayer.getTeamManager().getCurrentAvatarEntity();
             if (entity == null) return;
             Avatar avatar = entity.getAvatar();
-            this.setConstellation(avatar, constLevel);
+
+            this.setConstellation(targetPlayer, avatar, constLevel);
 
             CommandHandler.sendTranslatedMessage(sender, "commands.setConst.success", avatar.getAvatarData().getName(), constLevel);
         } catch (NumberFormatException ignored) {
@@ -41,15 +50,36 @@ public final class SetConstCommand implements CommandHandler {
         }
     }
 
-    private void setConstellation(Avatar avatar, int constLevel) {
-        List<Integer> talentIds = avatar.getSkillDepot().getTalents();
+    private void setConstellation(Player player, Avatar avatar, int constLevel) {
+        int currentConstLevel = avatar.getCoreProudSkillLevel();
+        IntArrayList talentIds = new IntArrayList(avatar.getSkillDepot().getTalents());
         Set<Integer> talentIdList = avatar.getTalentIdList();
 
         talentIdList.clear();
-        talentIdList.addAll(talentIds.stream().limit(constLevel).toList());
+        avatar.setCoreProudSkillLevel(0);
 
-        avatar.setCoreProudSkillLevel(constLevel);
-        avatar.recalcStats();
+        for(int talent = 0; talent < constLevel; talent++) {
+            AvatarTalentData talentData = GameData.getAvatarTalentDataMap().get(talentIds.getInt(talent));
+            int mainCostItemId = talentData.getMainCostItemId();
+
+            player.getInventory().addItem(mainCostItemId);
+            Grasscutter.getGameServer().getInventorySystem().unlockAvatarConstellation(player, avatar.getGuid());
+        }
+
+        // force player to reload scene when necessary
+        if (constLevel < currentConstLevel) {
+            World world = player.getWorld();
+            Scene scene = player.getScene();
+            Position pos = player.getPosition();
+
+            world.transferPlayerToScene(player, 1, pos);
+            world.transferPlayerToScene(player, scene.getId(), pos);
+            scene.broadcastPacket(new PacketSceneEntityAppearNotify(player));
+        }
+
+        // ensure that all changes are visible to the player
+        avatar.recalcConstellations();
+        avatar.recalcStats(true);
         avatar.save();
     }
 }
