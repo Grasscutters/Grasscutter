8ed8b96c62b6f6ff8442ad4c3427e94fce817712
KingRainbow44 <kobedo11@gmail.com>
2022-08-08 14:39:48 -0400

    Introduce a new application arguments parser & handler

---
+++ src/main/java/emu/grasscutter/utils/StartupArguments.java
@@ -0,0 +1,128 @@
+package emu.grasscutter.utils;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import emu.grasscutter.BuildConfig;
+import emu.grasscutter.Grasscutter;
+import emu.grasscutter.net.packet.PacketOpcodesUtils;
+import emu.grasscutter.tools.Tools;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import static emu.grasscutter.config.Configuration.DATA;
+
+/**
+ * A parser for start-up arguments.
+ */
+public final class StartupArguments {
+    private StartupArguments() {
+        // This class is not meant to be instantiated.
+    }
+
+    /* A map of parameter -> argument handler. */
+    private static final Map<String, Function<String, Boolean>> argumentHandlers = Map.of(
+        "-handbook", StartupArguments::generateHandbook,
+        "-dumppacketids", parameter -> {
+            PacketOpcodesUtils.dumpPacketIds(); return true;
+        },
+        "-gachamap", StartupArguments::generateGachaMap,
+        "-version", StartupArguments::printVersion,
+        "-debug", StartupArguments::enableDebug,
+        "-lang", parameter -> {
+            Grasscutter.setPreferredLanguage(parameter); return false;
+        },
+
+        // Aliases.
+        "-v", StartupArguments::printVersion,
+        "-debugall", StartupArguments::enableDebug
+    );
+
+    /**
+     * Parses the provided start-up arguments.
+     * @param args The application start-up arguments.
+     * @return If the application should exit.
+     */
+    public static boolean parse(String[] args) {
+        boolean exitEarly = false;
+
+        // Parse the arguments.
+        for(var input : args) {
+            var containsParameter = input.contains("=");
+
+            var argument = containsParameter ? input.split("=")[0] : input;
+            var handler = argumentHandlers.get(argument.toLowerCase());
+
+            if (handler != null) {
+                exitEarly |= handler.apply(containsParameter ? input.split("=")[1] : null);
+            }
+        }
+
+        return exitEarly;
+    }
+
+    /**
+     * Generates the GM Handbook.
+     * @param parameter Additional parameters.
+     * @return True to exit early.
+     */
+    private static boolean generateHandbook(String parameter) {
+        try {
+            Tools.createGmHandbook();
+        } catch (Exception exception) {
+            Grasscutter.getLogger().error("Failed to generate GM Handbook.", exception);
+        }
+
+        return true;
+    }
+
+    /**
+     * Generates the Gacha map.
+     * @param parameter Additional parameters.
+     * @return True to exit early.
+     */
+    private static boolean generateGachaMap(String parameter) {
+        try {
+            Tools.createGachaMapping(DATA("gacha_mappings.js"));
+        } catch (Exception exception) {
+            Grasscutter.getLogger().error("Failed to generate Gacha Map.", exception);
+        }
+
+        return true;
+    }
+
+    /**
+     * Prints the server version.
+     * @param parameter Additional parameters.
+     * @return True to exit early.
+     */
+    private static boolean printVersion(String parameter) {
+        System.out.println("Grasscutter version: " + BuildConfig.VERSION + "-" + BuildConfig.GIT_HASH); return true;
+    }
+
+    /**
+     * Enables debug logging.
+     * @param parameter Additional parameters.
+     * @return False to continue execution.
+     */
+    private static boolean enableDebug(String parameter) {
+        // Set the logger to debug.
+        Grasscutter.getLogger().setLevel(Level.DEBUG);
+        Grasscutter.getLogger().debug("The logger is now running in debug mode.");
+
+        // Change loggers to debug.
+        ((Logger) LoggerFactory.getLogger("express"))
+            .setLevel(Level.INFO);
+        ((Logger) LoggerFactory.getLogger("org.quartz"))
+            .setLevel(Level.INFO);
+        ((Logger) LoggerFactory.getLogger("org.reflections"))
+            .setLevel(Level.INFO);
+        ((Logger) LoggerFactory.getLogger("org.eclipse.jetty"))
+            .setLevel(Level.INFO);
+        ((Logger) LoggerFactory.getLogger("org.mongodb.driver"))
+            .setLevel(Level.INFO);
+
+        return false;
+    }
+}
