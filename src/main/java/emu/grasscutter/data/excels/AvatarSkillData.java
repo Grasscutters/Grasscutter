b2ae779c99e1884f15249538fe171f76b39bc660
AnimeGitB <AnimeGitB@bigblueball.in>
2022-08-21 20:52:18 +0930

    Make TalentCommand show off new Translate function

--- src/main/java/emu/grasscutter/data/excels/AvatarSkillData.java
+++ src/main/java/emu/grasscutter/data/excels/AvatarSkillData.java
@@ -1,65 +1,29 @@
 package emu.grasscutter.data.excels;
 
-import java.util.List;
-
 import emu.grasscutter.data.GameResource;
 import emu.grasscutter.data.ResourceType;
 import emu.grasscutter.data.ResourceType.LoadPriority;
 import emu.grasscutter.game.props.ElementType;
+import lombok.Getter;
 
 @ResourceType(name = "AvatarSkillExcelConfigData.json", loadPriority = LoadPriority.HIGHEST)
 public class AvatarSkillData extends GameResource {
-	private int id;
-    private	float cdTime;
-    private int costElemVal;
-    private int maxChargeNum;
-    private int triggerID;
-    private boolean isAttackCameraLock;
-    private int proudSkillGroupId;
-    private ElementType costElemType;
-    private long nameTextMapHash;
-    private String abilityName;
+    private int id;
+    @Getter private float cdTime;
+    @Getter private int costElemVal;
+    @Getter private int maxChargeNum;
+    @Getter private int triggerID;
+    @Getter private boolean isAttackCameraLock;
+    @Getter private int proudSkillGroupId;
+    @Getter private ElementType costElemType;
+    @Getter private long nameTextMapHash;
+    @Getter private long descTextMapHash;
+    @Getter private String abilityName;
 
     @Override
 	public int getId(){
         return this.id;
     }
-
-	public float getCdTime() {
-		return cdTime;
-	}
-
-	public int getCostElemVal() {
-		return costElemVal;
-	}
-
-	public int getMaxChargeNum() {
-		return maxChargeNum;
-	}
-
-	public int getTriggerID() {
-		return triggerID;
-	}
-
-	public boolean isIsAttackCameraLock() {
-		return isAttackCameraLock;
-	}
-
-	public int getProudSkillGroupId() {
-		return proudSkillGroupId;
-	}
-
-	public ElementType getCostElemType() {
-		return costElemType;
-	}
-
-	public long getNameTextMapHash() {
-		return nameTextMapHash;
-	}
-
-	public String getAbilityName() {
-		return abilityName;
-	}
 	
 	@Override
 	public void onLoad() {
