b72a9044e53fbc37cc238a340b68189574d1063d
hamusuke <daiki.esdfth12488@gmail.com>
2022-10-09 09:58:59 +0900

    Update src/main/java/emu/grasscutter/server/packet/recv/HandlerGetFriendShowNameCardInfoReq.java
    Co-authored-by: Luke H-W <Birdulon@users.noreply.github.com>

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerGetFriendShowNameCardInfoReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerGetFriendShowNameCardInfoReq.java
@@ -12,7 +12,7 @@ import emu.grasscutter.server.packet.send.PacketGetFriendShowNameCardInfoRsp;
 public class HandlerGetFriendShowNameCardInfoReq extends PacketHandler {
     @Override
     public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
-        GetFriendShowNameCardInfoReqOuterClass.GetFriendShowNameCardInfoReq req = GetFriendShowNameCardInfoReqOuterClass.GetFriendShowNameCardInfoReq.parseFrom(payload);
+        var req = GetFriendShowNameCardInfoReqOuterClass.GetFriendShowNameCardInfoReq.parseFrom(payload);
 
         int targetUid = req.getUid();
         Player target = session.getServer().getPlayerByUid(targetUid, true);
