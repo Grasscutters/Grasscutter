5fcfde743ba372afa26edffd32f9fe5a3adb81dc
Someone999 <HSMan2657@outlook.com>
2022-10-30 22:10:10 +0800

    Added web api key config.

--- src/main/java/emu/grasscutter/config/ConfigContainer.java
+++ src/main/java/emu/grasscutter/config/ConfigContainer.java
@@ -93,6 +93,7 @@ public class ConfigContainer {
     }
 
     public static class Server {
+        public String apiKey;
         public Set<Integer> debugWhitelist = Set.of();
         public Set<Integer> debugBlacklist = Set.of();
         public ServerRunMode runMode = ServerRunMode.HYBRID;
