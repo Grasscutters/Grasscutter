8707bc1405f3d61d3293fd5d96a2cf9b83f5e0db
NotThorny <thumperdog60@gmail.com>
2023-01-28 01:44:27 -0700

    Add 'seen unlock' packets

---
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerHomeModuleSeenReq.java
@@ -0,0 +1,20 @@
+package emu.grasscutter.server.packet.recv;
+
+import emu.grasscutter.net.packet.Opcodes;
+import emu.grasscutter.net.packet.PacketHandler;
+import emu.grasscutter.net.packet.PacketOpcodes;
+import emu.grasscutter.net.proto.HomeModuleSeenReqOuterClass.HomeModuleSeenReq;
+import emu.grasscutter.server.game.GameSession;
+import emu.grasscutter.server.packet.send.PacketHomeModuleSeenRsp;
+
+@Opcodes(PacketOpcodes.HomeModuleSeenReq)
+public class HandlerHomeModuleSeenReq extends PacketHandler {
+    @Override
+    public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
+        var req = HomeModuleSeenReq.parseFrom(payload);
+        var seen = req.getSeenModuleIdListList();
+
+        session.send(new PacketHomeModuleSeenRsp(seen));
+        
+    }
+}
