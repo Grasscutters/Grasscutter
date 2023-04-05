1d03100dccbf154a41cd794df24bb00d73fdb7aa
KingRainbow44 <kobedo11@gmail.com>
2023-04-04 21:41:06 -0400

    Add building for handbook
    also implement linting, ci for handbook, and change `.editorconfig` to do 2-line for .yml

--- src/handbook/vite.config.ts
+++ src/handbook/vite.config.ts
@@ -3,13 +3,15 @@
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
-import viteSvgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+import viteSvgr from "vite-plugin-svgr";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 import postcss from "./cfg/postcss.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ react(), tsconfigPaths(), viteSvgr() ],
+    plugins: [ react(), tsconfigPaths(), viteSvgr(), viteSingleFile() ],
     css: { postcss }
 });
