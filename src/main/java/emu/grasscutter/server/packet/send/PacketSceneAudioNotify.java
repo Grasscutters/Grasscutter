8c91d9eda701cc1c87f6e1a51cf38802c977bf8c
Moistcrafter <aswdfzxcvbhgtyyn805@gmail.com>
2023-04-09 20:17:03 -0700

    Add co-op lyre support

---
+++ src/main/java/emu/grasscutter/server/packet/send/PacketSceneAudioNotify.java
@@ -0,0 +1,23 @@
+package emu.grasscutter.server.packet.send;
+
+import emu.grasscutter.net.packet.BasePacket;
+import emu.grasscutter.net.packet.PacketOpcodes;
+import emu.grasscutter.net.proto.SceneAudioNotifyOuterClass;
+import java.util.List;
+
+public class PacketSceneAudioNotify extends BasePacket {
+
+    public PacketSceneAudioNotify(int sourceUid, List<Float> param2, List<String> param3, int type, List<Integer> param1) {
+        super(PacketOpcodes.SceneAudioNotify);
+
+        SceneAudioNotifyOuterClass.SceneAudioNotify proto = SceneAudioNotifyOuterClass.SceneAudioNotify.newBuilder()
+                .setSourceUid(sourceUid)
+                .addAllParam2(param2)
+                .addAllParam3(param3)
+                .setType(type)
+                .addAllParam1(param1)
+                .build();
+        
+        this.setData(proto);
+    }
+}
