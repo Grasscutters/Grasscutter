1661c42def876fb6391d49bd8fda2e3babe8726d
KingRainbow44 <kobedo11@gmail.com>
2023-04-10 22:04:47 -0400

    Implement give item & Implement configurable handbook endpoints

--- src/main/java/emu/grasscutter/server/http/documentation/HandbookHandler.java
+++ src/main/java/emu/grasscutter/server/http/documentation/HandbookHandler.java
@@ -4,11 +4,15 @@ import emu.grasscutter.Grasscutter;
 import emu.grasscutter.Grasscutter.ServerRunMode;
 import emu.grasscutter.data.GameData;
 import emu.grasscutter.game.avatar.Avatar;
+import emu.grasscutter.game.inventory.GameItem;
+import emu.grasscutter.game.props.ActionReason;
 import emu.grasscutter.server.http.Router;
 import emu.grasscutter.utils.FileUtils;
 import emu.grasscutter.utils.objects.HandbookBody;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import static emu.grasscutter.config.Configuration.HANDBOOK;
 
 /** Handles requests for the new GM Handbook. */
 public final class HandbookHandler implements Router {
@@ -21,23 +25,27 @@ public final class HandbookHandler implements Router {
      */
     public HandbookHandler() {
         this.handbook = FileUtils.readResource("/handbook.html");
-        this.serve = this.handbook.length > 0;
+        this.serve = HANDBOOK.enable && this.handbook.length > 0;
     }
 
     @Override
     public void applyRoutes(Javalin javalin) {
+        if (!this.serve) return;
+
         // The handbook content. (built from src/handbook)
         javalin.get("/handbook", this::serveHandbook);
 
         // Handbook control routes.
         javalin.post("/handbook/avatar", this::grantAvatar);
+        javalin.post("/handbook/item", this::giveItem);
     }
 
     /**
      * @return True if the server can execute handbook commands.
      */
     private boolean controlSupported() {
-        return Grasscutter.getRunMode() == ServerRunMode.HYBRID;
+        return HANDBOOK.enable &&
+            Grasscutter.getRunMode() == ServerRunMode.HYBRID;
     }
 
     /**
@@ -105,6 +113,61 @@ public final class HandbookHandler implements Router {
                 .build());
         } catch (NumberFormatException ignored) {
             ctx.status(500).result("Invalid player UID or avatar ID.");
+        } catch (Exception exception) {
+            ctx.status(500).result("An error occurred while granting the avatar.");
+            Grasscutter.getLogger().debug("A handbook command error occurred.", exception);
+        }
+    }
+
+    /**
+     * Gives an item to the user.
+     *
+     * @route POST /handbook/item
+     * @param ctx The Javalin request context.
+     */
+    private void giveItem(Context ctx) {
+        if (!this.controlSupported()) {
+            ctx.status(500).result("Handbook control not supported.");
+            return;
+        }
+
+        // Parse the request body into a class.
+        var request = ctx.bodyAsClass(HandbookBody.GiveItem.class);
+        // Validate the request.
+        if (request.getPlayer() == null || request.getItem() == null) {
+            ctx.status(400).result("Invalid request.");
+            return;
+        }
+
+        try {
+            // Parse the requested player.
+            var playerId = Integer.parseInt(request.getPlayer());
+            var player = Grasscutter.getGameServer().getPlayerByUid(playerId);
+
+            // Parse the requested item.
+            var itemId = Integer.parseInt(request.getItem());
+            var itemData = GameData.getItemDataMap().get(itemId);
+
+            // Validate the request.
+            if (player == null || itemData == null) {
+                ctx.status(400).result("Invalid player UID or item ID.");
+                return;
+            }
+
+            // Create the new item stack.
+            var itemStack = new GameItem(itemData, request.getAmount());
+            // Add the item to the inventory.
+            player.getInventory().addItem(itemStack, ActionReason.Gm);
+
+            ctx.json(HandbookBody.Response.builder()
+                .status(200)
+                .message("Item granted.")
+                .build());
+        } catch (NumberFormatException ignored) {
+            ctx.status(500).result("Invalid player UID or item ID.");
+        } catch (Exception exception) {
+            ctx.status(500).result("An error occurred while granting the item.");
+            Grasscutter.getLogger().debug("A handbook command error occurred.", exception);
         }
     }
 }
--- src/main/java/emu/grasscutter/utils/objects/HandbookBody.java
+++ src/main/java/emu/grasscutter/utils/objects/HandbookBody.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 /** HTTP request object for handbook controls. */
+@SuppressWarnings("FieldMayBeFinal")
 public interface HandbookBody {
     @Builder
     class Response {
@@ -19,5 +20,13 @@ public interface HandbookBody {
         private int level = 90; // Range between 1 - 90.
         private int constellations = 6; // Range between 0 - 6.
         private int talentLevels = 10; // Range between 1 - 15.
+    }
+
+    @Getter
+    class GiveItem {
+        private String player; // Parse into online player ID.
+        private String item; // Parse into item ID.
+
+        private int amount = 1; // Range between 1 - Long.MAX_VALUE.
     }
 }
