0d680a6310bebcba2566e818a1d241120ae1f1c8
KingRainbow44 <kobedo11@gmail.com>
2023-04-24 00:09:59 -0400

    Fix quest domains not showing up

--- src/main/java/emu/grasscutter/game/quest/GameQuest.java
+++ src/main/java/emu/grasscutter/game/quest/GameQuest.java
@@ -289,7 +289,7 @@ public class GameQuest {
      */
     public List<IntIntImmutablePair> getDungeonIds() {
         // Check if this quest is active.
-        if (this.state != QuestState.UNFINISHED) return List.of();
+        if (this.state != QuestState.QUEST_STATE_UNFINISHED) return List.of();
 
         return this.getQuestData().getFinishCond().stream()
                 .filter(cond -> cond.getType() == QuestContent.QUEST_CONTENT_ENTER_DUNGEON)
