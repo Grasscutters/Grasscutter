4a347b448514d90411c41437ef12707b5a9b1357
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-05-14 14:12:17 +0530

    Make text scale on Minicard

--- src/handbook/src/ui/widgets/MiniCard.tsx
+++ src/handbook/src/ui/widgets/MiniCard.tsx
@@ -19,6 +19,9 @@ interface IState {
 
 class MiniCard extends React.Component<IProps, IState> {
     loading: number | any;
+    
+    containerRef: React.RefObject<HTMLDivElement>;
+    textRef: React.RefObject<HTMLDivElement>;
 
     constructor(props: IProps) {
         super(props);
@@ -28,6 +31,9 @@ class MiniCard extends React.Component<IProps, IState> {
             icon: true,
             loaded: false
         };
+
+        this.containerRef = React.createRef();
+        this.textRef = React.createRef();
     }
 
     /**
@@ -42,8 +48,34 @@ class MiniCard extends React.Component<IProps, IState> {
         if (!this.state.loaded) this.replaceIcon();
     }
 
+    /**
+     * Adjusts the font size of the text to fit the container.
+     * @private
+     */
+    private adjustFontSize() {
+        const container = this.containerRef.current;
+        const text = this.textRef.current;
+    
+        if (!container || !text) {
+            return;
+        }
+    
+        const containerWidth = container.offsetWidth;
+        const textWidth = text.scrollWidth;
+    
+        const fontSize = parseFloat(window.getComputedStyle(text).fontSize);
+        const availableWidth = containerWidth - 10;
+        const scaleFactor = availableWidth / textWidth;
+    
+        if (scaleFactor < 1) {
+            const newFontSize = fontSize * scaleFactor;
+            text.style.fontSize = newFontSize + 'px';
+        }
+    }
+
     componentDidMount() {
         this.loading = setTimeout(this.forceReplace.bind(this), 1e3);
+        this.adjustFontSize();
     }
 
     componentWillUnmount() {
@@ -54,7 +86,7 @@ class MiniCard extends React.Component<IProps, IState> {
     render() {
         return (
             <div className={"MiniCard"} onClick={this.props.onClick}>
-                <div className={"MiniCard_Background"}>
+                <div className={"MiniCard_Background"} ref={this.containerRef}>
                     {this.state.icon && (
                         <img
                             className={"MiniCard_Icon"}
@@ -66,7 +98,7 @@ class MiniCard extends React.Component<IProps, IState> {
                     )}
 
                     {(!this.state.loaded || !this.state.icon) && (
-                        <p className={"MiniCard_Label"}>{this.props.data.name}</p>
+                        <p className={"MiniCard_Label"} ref={this.textRef}>{this.props.data.name}</p>
                     )}
                 </div>
             </div>
