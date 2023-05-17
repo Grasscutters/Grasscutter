6a0cb2732c55e21b465b6e95face1c3b133713f4
KingRainbow44 <kobedo11@gmail.com>
2023-05-17 00:24:40 -0400

    Fix logic of `SetPlayerPropReq`

--- src/main/java/emu/grasscutter/server/packet/send/PacketSetPlayerPropRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketSetPlayerPropRsp.java
@@ -2,17 +2,18 @@ package emu.grasscutter.server.packet.send;
 
 import emu.grasscutter.net.packet.BasePacket;
 import emu.grasscutter.net.packet.PacketOpcodes;
-import emu.grasscutter.net.proto.SetPlayerPropRspOuterClass;
+import emu.grasscutter.net.proto.SetPlayerPropRspOuterClass.SetPlayerPropRsp;
 
 public class PacketSetPlayerPropRsp extends BasePacket {
 
     public PacketSetPlayerPropRsp(int retCode) {
         super(PacketOpcodes.SetPlayerPropRsp);
-        SetPlayerPropRspOuterClass.SetPlayerPropRsp.Builder proto =
-                SetPlayerPropRspOuterClass.SetPlayerPropRsp.newBuilder();
+
+        var proto = SetPlayerPropRsp.newBuilder();
         if (retCode != 0) {
             proto.setRetcode(retCode);
         }
+
         this.setData(proto.build());
     }
 }
