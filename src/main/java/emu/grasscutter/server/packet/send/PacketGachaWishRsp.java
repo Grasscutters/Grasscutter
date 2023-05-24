600300ab5f5058675aed2faed6791172528b2462
KingRainbow44 <kobedo11@gmail.com>
2023-05-23 21:33:08 -0400

    Try to fix packet

--- src/main/java/emu/grasscutter/server/packet/send/PacketGachaWishRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGachaWishRsp.java
@@ -12,7 +12,7 @@ public class PacketGachaWishRsp extends BasePacket {
         GachaWishRsp proto = GachaWishRsp.newBuilder()
                 .setGachaType(gachaType)
                 .setGachaScheduleId(scheduleId)
-                .setWishItemId(itemId)
+                .setTenCostItemId(itemId)
                 .setWishProgress(progress)
                 .setWishMaxProgress(maxProgress)
                 .build();
