42be1ed6249067621963950f56ce9ab636bb2f51
KingRainbow44 <kobedo11@gmail.com>
2023-05-26 23:44:27 -0400

    Re-add field setters and add note on removal

--- src/main/java/emu/grasscutter/server/packet/send/PacketGetShopRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGetShopRsp.java
@@ -43,6 +43,10 @@ public class PacketGetShopRsp extends BasePacket {
                         .setMinLevel(info.getMinLevel())
                         .setMaxLevel(info.getMaxLevel())
                         .setMcoin(info.getMcoin());
+
+                        // These fields are deprecated as of REL3.7
+                        // .setDisableType(info.getDisableType())
+                        // .setSecondarySheetId(info.getSecondarySheetId());
                 if (info.getCostItemList() != null) {
                     goods.addAllCostItemList(info.getCostItemList().stream().map(x -> ItemParamOuterClass.ItemParam.newBuilder().setItemId(x.getId()).setCount(x.getCount()).build()).collect(Collectors.toList()));
                 }
