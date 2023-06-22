b305d93871d7bd66b3eb1657a77b1afdd7cb1a12
66hh <49398720+66hh@users.noreply.github.com>
2023-06-22 15:16:20 +0800

    import Collectors

--- src/main/java/emu/grasscutter/command/commands/ClearCommand.java
+++ src/main/java/emu/grasscutter/command/commands/ClearCommand.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.Setter;
 
