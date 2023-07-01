4e71a154793ed8e66076b0b17bffca049d2298b4
KingRainbow44 <kobedo11@gmail.com>
2023-07-01 01:29:14 -0400

    Move unknown condition handler message to debug

--- src/main/java/emu/grasscutter/game/activity/condition/all/UnknownActivityConditionHandler.java
+++ src/main/java/emu/grasscutter/game/activity/condition/all/UnknownActivityConditionHandler.java
@@ -15,7 +15,7 @@ public class UnknownActivityConditionHandler extends ActivityConditionBaseHandle
     @Override
     public boolean execute(
             PlayerActivityData activityData, ActivityConfigItem activityConfig, int... params) {
-        Grasscutter.getLogger().error("Called unknown condition handler {}.", conditions.name());
+        Grasscutter.getLogger().debug("Called unknown condition handler {}.", conditions.name());
         return false;
     }
 }
