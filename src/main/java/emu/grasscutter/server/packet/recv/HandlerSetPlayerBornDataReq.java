5a613b87767958417b23f146482c139c3523cf7a
Thoronium <107363768+NotThorny@users.noreply.github.com>
2023-08-27 20:58:24 -0600

    Remove unnecessary set

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerSetPlayerBornDataReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerSetPlayerBornDataReq.java
@@ -28,13 +28,10 @@ public class HandlerSetPlayerBornDataReq extends PacketHandler {
         // Sanity checks
         int avatarId = req.getAvatarId();
         int startingSkillDepot;
-        int elementlessSkillDepot;
         if (avatarId == GameConstants.MAIN_CHARACTER_MALE) {
             startingSkillDepot = 504;
-            elementlessSkillDepot = 501;
         } else if (avatarId == GameConstants.MAIN_CHARACTER_FEMALE) {
             startingSkillDepot = 704;
-            elementlessSkillDepot = 701;
         } else {
             return;
         }
@@ -59,9 +56,6 @@ public class HandlerSetPlayerBornDataReq extends PacketHandler {
             if (!GAME_OPTIONS.questing.enabled) {
                 mainCharacter.setSkillDepotData(
                         GameData.getAvatarSkillDepotDataMap().get(startingSkillDepot));
-            } else {
-                mainCharacter.setSkillDepotData(
-                        GameData.getAvatarSkillDepotDataMap().get(elementlessSkillDepot));
             }
 
             // Manually handle adding to team
