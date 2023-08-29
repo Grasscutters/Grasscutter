b72f81e7200eac2f9a6cccfa81b94b4742354d76
hamusuke <daiki.esdfth12488@gmail.com>
2023-08-30 01:18:39 +0900

    just sync it! (#2319)

---
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerMassiveEntityElementOpBatchNotify.java
@@ -0,0 +1,16 @@
+package emu.grasscutter.server.packet.recv;
+
+import emu.grasscutter.net.packet.Opcodes;
+import emu.grasscutter.net.packet.PacketHandler;
+import emu.grasscutter.net.packet.PacketOpcodes;
+import emu.grasscutter.net.proto.MassiveEntityElementOpBatchNotifyOuterClass;
+import emu.grasscutter.server.game.GameSession;
+import emu.grasscutter.server.packet.send.PacketMassiveEntityElementOpBatchNotify;
+
+@Opcodes(PacketOpcodes.MassiveEntityElementOpBatchNotify)
+public class HandlerMassiveEntityElementOpBatchNotify extends PacketHandler {
+    @Override
+    public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
+        session.getPlayer().getScene().broadcastPacketToOthers(session.getPlayer(), new PacketMassiveEntityElementOpBatchNotify(MassiveEntityElementOpBatchNotifyOuterClass.MassiveEntityElementOpBatchNotify.parseFrom(payload)));
+    }
+}
