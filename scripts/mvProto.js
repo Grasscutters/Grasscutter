cbb1d4ad9b47b47ab4d0341ac5e88fa4f85b9a20
2y8e9h22 <70102795+2y8e9h22@users.noreply.github.com>
2023-09-03 16:30:25 +0800

    update

--- scripts/mvProto.js
+++ scripts/mvProto.js
@@ -1,24 +1,28 @@
 const fs = require('node:fs')
 const path = require('node:path')
 
-function match(p, n) {
+function search(p, n, o) {
+    let c = fs.readFileSync(p, 'utf8')
+    for (let i = n.length - 1; i > -1; i--) {
+        if (c.includes(n[i])) {
+            o.push(...n.splice(i, 1))
+        }
+    }
+}
+
+function match(p, n, o) {
 	for (let f of fs.readdirSync(p, { withFileTypes: true })) {
         let fp = path.join(f.path, f.name)
         if (f.isDirectory()) {
-	        n = match(fp, n)
+	        match(fp, n, o)
             continue
         }
         if (!f.isFile()) {
             continue
 	    }
-        let c = fs.readFileSync(fp, 'utf8')
-        for (let i = n.length - 1; i > -1; i--) {
-            if (c.includes(n[i])) {
-                n.splice(i, 1)
-            }
-        }
+	    search(fp, n, o)
 	}
-	return n
+	return o
 }
 
 function move(p1, p2, p3) {
@@ -26,43 +30,24 @@ function move(p1, p2, p3) {
     console.info('路径', '=>', p1, '=>', p2)
 	let n = fs.readdirSync(p1).map(x => x.replace('.java', ''))
     console.info('数量', '=>', n.length)
-	let p = match(p2, n)
-    console.info('无效', '=>', p.length)
-    console.info('移动', '=>', p1, '=>', p3)
-    console.info('-----------------------------')
-    for (let v of p) {
-        v += '.java'
-        fs.renameSync(path.join(p1, v), path.join(p3, v))
-    }
-}
-
-function fix(p1, p3) {
-    console.info('路径', '=>', p3, '=>', p1)
-	let n = fs.readdirSync(p3).map(x => x.replace('.java', ''))
-    console.info('数量', '=>', n.length)
-    let f = (p, m) => {
-        let o = []
+    let f = (p, m, o) => {
         for (let v of m) {
-            let c = fs.readFileSync(path.join(p, v + '.java'), 'utf8')
-            for (let i = n.length - 1; i > -1; i--) {
-                if (c.includes(n[i])) {
-                    o.push(...n.splice(i, 1))
-                }
-            }
+            search(path.join(p, v + '.java'), n, o)
         }
         if (o.length) {
-            o.push(...f(p3, o))
+            o.push(...f(p1, o, []))
         }
         return o
     }
-	let k = fs.readdirSync(p1).map(x => x.replace('.java', ''))
-    let m = f(p1, k)
-    console.info('有效', '=>', m.length)
-    console.info('移动', '=>', p3, '=>', p1)
+    let p = match(p2, n, [])
+    p.push(...f(p1, p, []))
+    console.info('有效', '=>', p.length)
+    console.info('无效', '=>', n.length)
+    console.info('移动', '=>', p1, '=>', p3)
     console.info('-----------------------------')
-    for (let v of m) {
+    for (let v of n) {
         v += '.java'
-        fs.renameSync(path.join(p3, v), path.join(p1, v))
+        fs.renameSync(path.join(p1, v), path.join(p3, v))
     }
 }
 
@@ -72,7 +57,6 @@ function main(p1, p2) {
 	    fs.mkdirSync(p3)
 	}
 	move(p1, p2, p3)
-    fix(p1, p3)
 }
 
 //by 2y8e9h22
