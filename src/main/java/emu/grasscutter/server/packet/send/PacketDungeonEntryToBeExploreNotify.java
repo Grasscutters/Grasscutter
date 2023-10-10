4497cec9be2c7ec1acf9097865f183677dcb23b5
longfruit <voltydragonfruit@outlook.com>
2023-10-10 09:20:09 -0700

    add note to DungeonEntryToBeExploreNotify

--- src/main/java/emu/grasscutter/server/packet/send/PacketDungeonEntryToBeExploreNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketDungeonEntryToBeExploreNotify.java
@@ -4,6 +4,11 @@ import emu.grasscutter.net.packet.*;
 import emu.grasscutter.net.proto.DungeonEntryToBeExploreNotifyOuterClass.DungeonEntryToBeExploreNotify;
 
 public class PacketDungeonEntryToBeExploreNotify extends BasePacket {
+
+    /**
+     * Marks the dungeon as pending exploration.
+     * This creates the "Unknown" text bubble above the dungeon entry in the world map.
+     */
     public PacketDungeonEntryToBeExploreNotify(int dungeonEntryScenePointId, int sceneId, int dungeonEntryConfigId) {
         super(PacketOpcodes.DungeonEntryToBeExploreNotify);
         this.setData(DungeonEntryToBeExploreNotify.newBuilder()
